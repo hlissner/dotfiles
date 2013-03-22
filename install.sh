a57e94a9125140c67c361c5e297b500c4d88230d
Henrik Lissner <henrik@lissner.net>
2013-03-21 22:26:04 -0400

    Refactor install scripts (KISS)

--- install.sh
+++ install.sh
@@ -5,20 +5,19 @@ RS=$(tput sgr0)
 
 src=$(cd "$(dirname "$0")"; pwd)
 if [ ! -d $src/install ]; then
-    echo -e "$GREEN==> Cloning dotfiles repo"
+    echo -e "$GREEN==> Cloning dotfiles repo$RS"
     git clone https://github.com/hlissner/dotfiles.git ~/.dotfiles | sed "s/^/  /"
     src="~/.dotfiles"
 fi
 
-{
-    $src/install/osx
-    $src/install/zsh
-    $src/install/brew
-    $src/install/git
-    $src/install/rvm
-    $src/install/tmux
-} | sed "s/^/  /"
+$src/install/osx
+$src/install/zsh
+$src/install/brew
+$src/install/git
+$src/install/rvm
+$src/install/tmux
 
-echo "Done!"
+echo
+echo "${GREEN}Done!$RS"
 echo "And if you want to install vim, run:"
 echo "   sh <(curl https://raw.github.com/hlissner/mlvim/master/install.sh -L)"
