1683fc23aaa75af2938d0bdc00cf9f7ff19db3c8
Henrik Lissner <henrik@lissner.net>
2014-06-21 04:56:19 -0400

    Clean up install script

--- install.sh
+++ install.sh
@@ -2,18 +2,19 @@
 
 function do_link {
     if [ ! -h "$2" ]; then
-        ln -sf "~/.dotfiles/$1" "$2"
+        echo "ln -sf '$HOME/.dotfiles/$1' '$HOME/.$1'"
+        ln -sf "$HOME/.dotfiles/$1" "$HOME/.$1"
     fi
 }
 
 [ ! -d ~/.dotfiles ] && git clone git@github.com:hlissner/dotfiles.git ~/.dotfiles
 
-do_link zsh $HOME/.zsh
-do_link zshrc $HOME/.zshrc
-do_link zshenv $HOME/.zshenv
-do_link tmux.conf $HOME/.tmux.conf
-do_link gitignore $HOME/.gitignore
-do_link gitconfig $HOME/.gitconfig
-do_link bashrc $HOME/.bashrc
-do_link ctags $HOME/.ctags
-do_link rake $HOME/.rake
+do_link zsh
+do_link zshrc
+do_link zshenv
+do_link tmux.conf
+do_link gitignore
+do_link gitconfig
+do_link bashrc
+do_link ctags
+do_link rake
