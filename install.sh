a368ecb2f8db9b99955973a1c3b9bd29071846d2
Henrik Lissner <henrik@lissner.net>
2015-07-04 02:06:22 +0200

    Remove vestiges of zprezto

--- install.sh
+++ install.sh
@@ -2,11 +2,6 @@
 
 # In case this file was run via curl
 [ ! -d ~/.dotfiles ] && git clone https://github.com/hlissner/dotfiles ~/.dotfiles
-[ ! -d ~/.zprezto ] && git clone --recursive https://github.com/hlissner/prezto ~/.zprezto
-if [ ! -d ~/.dotfiles/vim/bundle/neobundle.vim ]; then
-    mkdir -p ~/.dotfiles/vim/bundle
-    git clone --recursive https://github.com/Shougo/neobundle.vim ~/.dotfiles/vim/bundle/neobundle.vim
-fi
 
 shopt -s extglob
 for rcfile in "$HOME"/.dotfiles/!(bin|README.md|Gemfile*|install.sh); do
