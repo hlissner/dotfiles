1a7ae1317638c602dad5a0e0e5129d2021322933
Henrik Lissner <henrik@lissner.net>
2015-12-13 06:45:16 -0500

    zsh: host-specific caches

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,10 +2,13 @@ is-callable() {
     (( $+commands[$1] )) || (( $+functions[$1] )) || (( $+aliases[$1] ))
 }
 
+ZSH_CACHE_DIR="$HOME/.zsh/cache/$(hostname)"
+[[ ! -d "$ZSH_CACHE_DIR" ]] && mkdir -p "$ZSH_CACHE_DIR"
+
 cache() {
     is-callable "$1" || return 1
-    local cache="$HOME/.zsh/cache/$1-cache"
-    if [ ! -s "$cache" ]; then
+    local cache="${ZSH_CACHE_DIR}/$1-cache"
+    if [[ ! -s "$cache" ]]; then
         echo "Caching $1"
         $@ >| "$cache" 2> /dev/null
     fi
