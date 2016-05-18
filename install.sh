46990de65c25e3e7c89c8a48343a8badb68d91cd
Henrik Lissner <henrik@lissner.net>
2016-05-18 04:30:06 -0400

    install.sh: don't install hammerspoon dotfiles on non-OSX machines

--- install.sh
+++ install.sh
@@ -4,10 +4,13 @@ _clone() { [ -d "$2" ] || git clone --recursive "$1" "$2"; }
 
 # In case this file was run via curl
 _clone https://github.com/hlissner/dotfiles ~/.dotfiles
-_clone https://github.com/hlissner/.hammerspoon ~/.hammerspoon
 _clone https://github.com/hlissner/.vim ~/.vim
 _clone https://github.com/tarjoilija/zgen ~/.zgen
 _clone https://github.com/jimeh/tmuxifier ~/.tmuxifier
+
+if [ "$(name)" == "Darwin" ]; then
+    _clone https://github.com/hlissner/.hammerspoon ~/.hammerspoon
+fi
 
 shopt -s extglob
 for rcfile in "${HOME}"/.dotfiles/!(.*|bin|scripts|*.md|install.sh)
