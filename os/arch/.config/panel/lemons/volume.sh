e2fa956571ba8bdf0d4d6276765a39f0ec17e104
Henrik Lissner <henrik@lissner.net>
2016-10-20 22:47:57 +0200

    panel > volume lemon: only initialize once (leave the rest to volume.sh)

--- os/arch/.config/panel/lemons/volume.sh
+++ os/arch/.config/panel/lemons/volume.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-# volume messages will be sent when volume is changed
-echo "V$(amixer get Master | sed -n 's/^.*\[\([0-9]\+\)%.*$/\1/p' | uniq)" > "$PANEL_FIFO" &
-
-# while :
-# do
-#     echo "V$(amixer get Master | sed -n 's/^.*\[\([0-9]\+\)%.*$/\1/p' | uniq)"
-#     sleep 1
-# done > "$PANEL_FIFO" &
+# initialize volume display (once)
+echo "V$(volume.sh)" > "$PANEL_FIFO" &
