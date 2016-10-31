ea5446105dd6d0640a4d348a1fb8d36134cf01a9
Henrik Lissner <henrik@lissner.net>
2016-10-31 15:31:39 +0100

    panel: refresh volume every 3s

--- os/arch/.config/panel/lemons/volume.sh
+++ os/arch/.config/panel/lemons/volume.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-# initialize volume display (once)
-echo "V$(volume.sh)" > "$PANEL_FIFO" &
+while :
+do
+    echo "V$(volume.sh)" > "$PANEL_FIFO" &
+    sleep 3
+done > "$PANEL_FIFO" &
