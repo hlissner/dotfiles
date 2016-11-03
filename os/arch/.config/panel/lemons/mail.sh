637225311a825d25f84d687fa873c3966a8cbb3f
Henrik Lissner <henrik@lissner.net>
2016-11-03 18:45:53 +0100

    panel/lemons: mail.sh now considers all my accounts

--- os/arch/.config/panel/lemons/mail.sh
+++ os/arch/.config/panel/lemons/mail.sh
@@ -2,22 +2,19 @@
 
 [ -f ~/.secrets/accounts.sh ] && . ~/.secrets/accounts.sh || exit
 
-feed=$(curl -u "${MAIL_USER}:${MAIL_PASS}" --silent https://mail.google.com/mail/feed/atom)
-con=$(echo $feed | grep -c "<fullcount>")
-num=$(echo $feed | grep -o "<entry>" | wc -l)
+get-mail() {
+    local feed=$(curl -u "$1:$2" --silent https://mail.google.com/mail/feed/atom)
+    if [ "$(echo $feed | grep -c "<fullcount>")" -eq 1 ]
+    then
+        echo $feed | grep -o "<entry>" | wc -l
+    else
+        >&2 echo "mail.sh: error with $1"
+        echo 0
+    fi
+}
 
-if [ "$con" -eq 1 ]
-then
-    # if [ "$num" -gt 0 ]; then
-        # oldnum=0
-        # [ -f /tmp/newmail ] && oldnum=$(cat /tmp/newmail)
-        # echo "$num" > /tmp/newmail
-        # if [ "$num" -ne "$oldnum" ]
-        # then
-        #     notify-send 'New email'
-        # fi
-    # fi
-    echo "M$num"
-else
-    echo "M!"
-fi
+a=$(get-mail "${MAIL1[USER]}" "${MAIL1[PASS]}")
+b=$(get-mail "${MAIL2[USER]}" "${MAIL2[PASS]}")
+c=$(get-mail "${MAIL3[USER]}" "${MAIL3[PASS]}")
+
+echo M$(( $a + $b + $c ))
