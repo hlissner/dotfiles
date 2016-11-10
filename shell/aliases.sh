b9b28c87010a228ee9afed9ad7d03cfb92187964
Henrik Lissner <henrik@lissner.net>
2016-11-10 18:35:10 +0100

    shell/aliases: add pubkey, map, take and hex aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -7,8 +7,16 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 alias -- -='cd -'
 
+alias ln="${aliases[ln]:-ln} -v"  # verbose ln
 alias ls="${aliases[ls]:-ls} --group-directories-first"
 alias l='ls -l'
 alias ll='ls -la'
 
-alias ln='ln -v'  # verbose ln
+alias pubkey='cat ~/.ssh/id_rsa.pub | y | echo "Public key copied to pasteboard."'
+
+# For example, to list all directories that contain a certain file: find . -name
+# .gitattributes | map dirname
+alias map="xargs -n1"
+
+take() { mkdir "$1" && cd "$1"; }
+hex()  { echo -n $@ | xxd -psdu; }
