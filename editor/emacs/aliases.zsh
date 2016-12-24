9140387a74920bf0bbfceaf7c7440f098bbe4093
Henrik Lissner <henrik@lissner.net>
2016-12-24 14:38:54 -0500

    editor/emacs: refactor aliases.zsh

--- editor/emacs/aliases.zsh
+++ editor/emacs/aliases.zsh
@@ -1,16 +1,15 @@
-if is-callable emacs; then
-    if [[ "$OSTYPE" == darwin* ]]; then
-        alias emacs='emacs -nw'
-        e() { # Emacs version of v
-            if pgrep Emacs 2>&1 >/dev/null; then
-                emacsclient -n $@
-            else
-                /Applications/Emacs.app/Contents/MacOS/Emacs $@ &
-            fi
-        }
-    else
-        alias e='emacsclient -n'
-    fi
+if [[ "$OSTYPE" == darwin* ]]; then
+    alias emacs='emacs -nw'
 
-    ediff() { e --eval "(ediff-files \"$1\" \"$2\")"; }
+    e() { # Emacs version of v
+        if pgrep Emacs 2>&1 >/dev/null; then
+            emacsclient -n $@
+        else
+            /Applications/Emacs.app/Contents/MacOS/Emacs $@ &
+        fi
+    }
+else
+    alias e='emacsclient -n'
 fi
+
+ediff() { e --eval "(ediff-files \"$1\" \"$2\")"; }
