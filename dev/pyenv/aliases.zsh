37d376502b4bf727f111a8e6e0f9844edf23972a
Henrik Lissner <henrik@lissner.net>
2016-12-23 19:49:09 -0500

    ruby=>rbenv, python=>pyenv; add install+update scripts

---
+++ dev/pyenv/aliases.zsh
@@ -0,0 +1,6 @@
+alias py='python'
+command -v python2 >/dev/null && alias py2='python2'
+command -v python3 >/dev/null && alias py3='python3'
+
+alias pye='pyenv'
+alias ipy='ipython'
--- dev/python/aliases.zsh
+++
@@ -1,5 +0,0 @@
-alias py='python'
-alias py2='python2'
-alias py3='python3'
-alias pye='pyenv'
-alias ipy='ipython'
