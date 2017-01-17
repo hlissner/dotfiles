c9c4b4addda1fe2c031732261f859bb676d934d8
Henrik Lissner <henrik@lissner.net>
2017-01-16 22:30:02 -0500

    shell/env.sh: move PATH init close to populator

--- shell/env.sh
+++ shell/env.sh
@@ -1,7 +1,5 @@
 # Loaded at the end of the initialization process
 
-PATH="$HOME/.bin:$DOTFILES/bin:$PATH"
-
 #
 export EDITOR=$(is-callable nvim && echo 'nvim' || echo 'vim')
 export VISUAL=$EDITOR
@@ -9,9 +7,9 @@ export VISUAL=$EDITOR
 #
 export GPG_TTY=$(tty)
 
+PATH="$HOME/.bin:$DOTFILES/bin:$PATH"
 local bins=( "$ENABLED_DIR"/*/bin )
 for bin in "${bins[@]}"; do
     [[ -e "$bin" ]] && PATH="$bin:$PATH"
 done
-
 export PATH
