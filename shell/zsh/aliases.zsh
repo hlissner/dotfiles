216d1d5ec94cb2dce12d131e1a50e24dda41bb33
Henrik Lissner <henrik@lissner.net>
2017-04-25 02:35:22 -0400

    Add bin/duckduckgo & ddg alias

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -6,6 +6,8 @@ autoload -U zmv
 zman() { PAGER="less -g -s '+/^       "$1"'" man zshall; }
 alias ag='noglob ag'
 
+alias ddg='duckduckgo'
+
 # Canonical hex dump; some systems have this symlinked
 is-callable hd || alias hd="hexdump -C"
 
