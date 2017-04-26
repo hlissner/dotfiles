6fd52171e03523c6b64a298a269765ae539138a5
Henrik Lissner <henrik@lissner.net>
2017-04-26 18:56:00 -0400

    +weechat: use pass to set up secure variables

--- +weechat/scripts/init.sh
+++ +weechat/scripts/init.sh
@@ -9,7 +9,15 @@ if [[ -f ~/.private/config/irc.conf ]]; then
     # ...otherwise, configure it!
     CWD=$(cd $(dirname $0) && pwd -P)
 
-    wee "$HOME/.private/config/irc.conf"
+    wee "/secure passphrase $(pass irc/master)"
+    wee "/secure set username vnought"
+    wee "/secure set freenode $(pass irc/freenode.net | head -n1)"
+    wee "/secure set snoonet $(pass irc/snoonet.org | head -n1)"
+    wee "/secure set oftc $(pass irc/oftc.net | head -n1)"
+    wee "/secure set afternet $(pass irc/afternet.net | head -n1)"
+    wee "/secure set pushover_key $(pass api/pushover/key)"
+    wee "/secure set pushover_api $(pass api/pushover/api)"
+
     wee "$CWD/../plugins.txt"
     wee "$CWD/../settings.txt"
     wee "$CWD/../servers.txt"
