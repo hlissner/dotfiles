d84ea2a138ae94824aea080a67e9e29a69ca58c0
Henrik Lissner <henrik@lissner.net>
2017-08-06 16:09:19 +0200

    shell/+zsh: add youtube-dl aliases

--- shell/+zsh/aliases.zsh
+++ shell/+zsh/aliases.zsh
@@ -8,6 +8,8 @@ alias ag='noglob ag'
 
 alias ddg='duckduckgo'
 alias bt='transmission-remote'
+alias ydl='youtube-dl'
+alias ydls='youtube-dl --extract-audio --audio-format m4a'
 
 # Canonical hex dump; some systems have this symlinked
 is-callable hd || alias hd="hexdump -C"
