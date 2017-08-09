d1da84d0aaf2760bee12e2cf5bb839aa7c49ebc6
Henrik Lissner <henrik@lissner.net>
2017-08-09 15:32:37 +0200

    os/+arch: BROWSER=firefox; add ~/.local/bin to PATH

--- os/+arch/env.zsh
+++ os/+arch/env.zsh
@@ -1,4 +1,5 @@
-export BROWSER=chromium
+path=(~/.local/bin $path)
 
+export BROWSER=firefox
 [[ -f ~/.ssh/agentenv ]]   && source ~/.ssh/agentenv
 [[ -f ~/.gnupg/agentenv ]] && source ~/.gnupg/agentenv
