61ceb57fbf6c4a93ad6884979eac5dbc9b3428b8
Henrik Lissner <henrik@lissner.net>
2017-09-05 19:15:56 +0200

    shell/+mpd: add mpd-add script

--- shell/+mpd/aliases.zsh
+++ shell/+mpd/aliases.zsh
@@ -2,11 +2,3 @@
 if [[ $OSTYPE == darwin* ]]; then
     alias mpdi='mpd ~/.config/mpd/mpd.conf'
 fi
-
-mpca() {
-    local files=()
-    for file in $@; do
-        files+="$(realpath --relative-to="$HOME/music" "$file")"
-    done
-    mpc add ${files[@]}
-}
