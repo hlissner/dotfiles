56ac50129f00f886ff5a5f6cfd3022ea7a3d601c
Henrik Lissner <henrik@lissner.net>
2017-10-03 19:14:49 +0200

    Reorganize zsh aliases

--- shell/+zsh/aliases.zsh
+++ shell/+zsh/aliases.zsh
@@ -6,10 +6,6 @@ autoload -U zmv
 zman() { PAGER="less -g -s '+/^       "$1"'" man zshall; }
 alias ag='noglob ag'
 
-alias ddg='duckduckgo'
-alias bt='transmission-remote'
-alias ydl='youtube-dl'
-alias ydls='youtube-dl --extract-audio --audio-format m4a'
 remind() {
   local time=$1; shift
   sched $time "notify-send --urgency=critical 'Reminder' '$@'; ding";
--- shell/aliases.sh
+++ shell/aliases.sh
@@ -28,9 +28,6 @@ alias wget='wget -c'                # Resume dl if possible
 
 alias pubkey='cat ~/.ssh/id_rsa.pub'
 
-alias yd='youtube-dl'
-alias ydm='youtube-dl --extract-audio --audio-format aac'
-
 # Always enable colored `grep` output
 # Note: `GREP_OPTIONS="--color=auto"` is deprecated, hence the alias usage.
 alias grep='grep --color=auto'
@@ -43,6 +40,13 @@ alias exe='exercism'
 # For example, to list all directories that contain a certain file: find . -name
 # .gitattributes | map dirname
 alias map="xargs -n1"
+
+alias ydl='youtube-dl'
+alias ydl-aac='youtube-dl --extract-audio --audio-format aac'
+alias ydl-m4a='youtube-dl --extract-audio --audio-format m4a'
+
+alias ddg='duckduckgo'
+alias bt='transmission-remote'
 
 take() { mkdir "$1" && cd "$1"; }
 hex()  { echo -n $@ | xxd -psdu; }
