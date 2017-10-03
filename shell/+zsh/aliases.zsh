db442d77022ddb0f635aa296cbbeba055bb92b97
Henrik Lissner <henrik@lissner.net>
2017-10-03 19:14:28 +0200

    Replace os/+arch/bin/remind with sched alias

--- shell/+zsh/aliases.zsh
+++ shell/+zsh/aliases.zsh
@@ -10,6 +10,11 @@ alias ddg='duckduckgo'
 alias bt='transmission-remote'
 alias ydl='youtube-dl'
 alias ydls='youtube-dl --extract-audio --audio-format m4a'
+remind() {
+  local time=$1; shift
+  sched $time "notify-send --urgency=critical 'Reminder' '$@'; ding";
+}
+alias r='remind'
 
 # Canonical hex dump; some systems have this symlinked
 is-callable hd || alias hd="hexdump -C"
