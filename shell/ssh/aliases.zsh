ea325500572df579f9e1137728360e551961c77a
Henrik Lissner <henrik@lissner.net>
2017-12-18 01:13:05 -0500

    Delete shell/ssh topic

--- shell/ssh/aliases.zsh
+++
@@ -1,9 +0,0 @@
-key="/keybase/private/$USER/id_rsa"
-
-config="-o UserKnownHostsFile='$XDG_CONFIG_HOME/ssh/known_hosts' -o IdentityFile='$key'"
-[[ -f $XDG_CONFIG_HOME/ssh/config ]] && config="$config -F'$XDG_CONFIG_HOME/ssh/config"
-
-alias ssh="ssh $config "
-alias scp="scp -o GlobalKnownHostsFile=/etc/hosts $config "
-alias ssh-copy-id="ssh-copy-id -i $key"
-alias ssha="ssh-add $key"
