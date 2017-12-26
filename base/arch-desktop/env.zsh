b903465759574e1ae0f03423ed1b4c1b5268d9c8
Henrik Lissner <henrik@lissner.net>
2017-12-26 12:51:59 -0500

    base/arch-desktop: adwaita => arc-dark & use gtk theme in qt5

--- base/arch-desktop/env.zsh
+++ base/arch-desktop/env.zsh
@@ -2,3 +2,4 @@ export BROWSER=firefox
 export GNUPGHOME="$XDG_CONFIG_HOME/gpg"
 export GTK2_RC_FILES="${0:A:h}/config/gtk-2.0/gtkrc"
 export URXVT_PERL_LIB="$XDG_CONFIG_HOME/urxvt"
+export QT_QPA_PLATFORMTHEME=gtk2
