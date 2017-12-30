5b7bff051675c04eb7002d411f79875241fb9fcc
Henrik Lissner <henrik@lissner.net>
2017-12-30 18:39:33 -0500

    Move ssh/gpg agent env init from arch => arch-desktop

--- base/arch-desktop/env.zsh
+++ base/arch-desktop/env.zsh
@@ -3,3 +3,12 @@ export GNUPGHOME="$XDG_CONFIG_HOME/gpg"
 export GTK2_RC_FILES="${0:A:h}/config/gtk-2.0/gtkrc"
 export URXVT_PERL_LIB="$XDG_CONFIG_HOME/urxvt"
 export QT_QPA_PLATFORMTHEME=gtk2
+
+_is_running ssh-agent || {
+  ssh-agent -s >"$XDG_RUNTIME_DIR/ssh-agent-env"
+}
+_is_running gpg-agent || {
+  gpg-agent --daemon >"$XDG_RUNTIME_DIR/gpg-agent-env"
+}
+_source "$XDG_RUNTIME_DIR/ssh-agent-env" >/dev/null
+_source "$XDG_RUNTIME_DIR/gpg-agent-env" >/dev/null
--- base/arch/env.zsh
+++ base/arch/env.zsh
@@ -1,11 +1,2 @@
 export GNUPGHOME="$XDG_CONFIG_HOME/gpg"
 export TERMINFO="$XDG_CONFIG_HOME/terminfo"
-
-_is_running ssh-agent || {
-  ssh-agent -s >"$XDG_RUNTIME_DIR/ssh-agent-env"
-}
-_is_running gpg-agent || {
-  gpg-agent --daemon >"$XDG_RUNTIME_DIR/gpg-agent-env"
-}
-_source "$XDG_RUNTIME_DIR/ssh-agent-env" >/dev/null
-_source "$XDG_RUNTIME_DIR/gpg-agent-env" >/dev/null
