9606cc70a6c24afe1c4e9639bee1a39626809ae8
Henrik Lissner <henrik@lissner.net>
2018-02-22 19:16:08 -0500

    misc/docker: add dkl, dkcl, dkclr, & dke aliases

--- misc/docker/aliases.zsh
+++ misc/docker/aliases.zsh
@@ -3,6 +3,17 @@ alias docker-compose='sudo docker-compose'
 
 alias dkc=docker-compose
 alias dkm=docker-machine
+alias dkl='dk logs'
+alias dkcl='dkc logs'
+
+dkclr() {
+  dk stop $(docker ps -a -q)
+  dk rm $(docker ps -a -q)
+}
+
+dke() {
+  dk exec -it "$1" "${@:1}"
+}
 
 dk() {
   case $(_os) in
