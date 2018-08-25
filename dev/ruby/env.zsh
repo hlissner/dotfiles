ece4123f77f23f3e671b480da2bd9f09f83f827d
Henrik Lissner <henrik@lissner.net>
2018-08-25 20:24:40 +0200

    dev/ruby: remove GEM_PATH, add $GEM_HOME/bin to PATH

--- dev/ruby/env.zsh
+++ dev/ruby/env.zsh
@@ -1,11 +1,10 @@
 export GEMRC="$XDG_CONFIG_HOME/gems/rc:$GEMRC"
 export GEM_HOME="$XDG_DATA_HOME/gems"
-export GEM_PATH="$XDG_BIN_HOME"
 export GEM_SPEC_CACHE="$XDG_CACHE_HOME/gem/specs"
 export IRBRC="$XDG_CONFIG_HOME/irb/rc"
 export PRYRC="$XDG_CONFIG_HOME/pry/rc"
 export SPEC_OPTS="--color --order random"
 
 export RBENV_ROOT="$XDG_DATA_HOME/rbenv"
-path=( $RBENV_ROOT/bin $path )
+path=( $RBENV_ROOT/bin $GEM_HOME/bin $path )
 _cache rbenv init - --no-rehash
