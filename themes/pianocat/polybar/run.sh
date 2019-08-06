aae79d06e3ad9a4fa74075b9f4f1bcc290452406
Henrik Lissner <henrik@lissner.net>
2019-08-06 14:29:33 -0400

    Add polybar config to pianocat theme

---
+++ themes/pianocat/polybar/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar main >>$XDG_DATA_HOME/polybar.log 2>&1 &
+echo 'Polybar launched...'
