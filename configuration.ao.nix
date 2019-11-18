1596653ffc10d6a0deb636201b0224b8475fcf6b
Henrik Lissner <henrik@lissner.net>
2019-11-18 01:26:14 -0500

    ao: add nginx config

--- configuration.ao.nix
+++ configuration.ao.nix
@@ -16,6 +16,38 @@
   networking.hostName = "ao";
   networking.networkmanager.enable = true;
 
+  # syncthing
   services.syncthing.guiAddress = "192.168.1.10:8384";
   networking.firewall.allowedTCPPorts = [ 8384 ];
+
+  # nginx hosts
+  services.nginx.virtualHosts = {
+    "v0.io" = {
+      default = true;
+      enableACME = true;
+      forceSSL = true;
+      root = "/home/hlissner/www/v0.io";
+    };
+    "dl.v0.io" = {
+      enableACME = true;
+      addSSL = true;
+      root = "/home/hlissner/www/dl.v0.io";
+      locations."~* \.(?:ico|css|js|gif|jpe?g|png|mp4)$".extraConfig = ''
+        expires 30d;
+        add_header Pragma public;
+        add_header Cache-Control public;
+      '';
+    };
+    # "git.v0.io" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/".proxyPass = http://gitea;
+    # };
+  };
+
+  services.acme.certs = {
+    "v0.io".email = "henrik@lissner.net";
+    "dl.v0.io".email = "henrik@lissner.net";
+    # "git.v0.io".email = "henrik@lissner.net";
+  };
 }
