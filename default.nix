a09d5d626eaa1078dcba1ad411bed2d36f43adf8
Henrik Lissner <henrik@lissner.net>
2019-11-17 23:25:47 -0500

    Enable firewall

--- default.nix
+++ default.nix
@@ -78,6 +78,8 @@
     home.file."bin" = { source = ./bin; recursive = true; };
   };
 
+  networking.firewall.enable = true;
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
