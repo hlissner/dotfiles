b5557d7d5a34f5ee25db73524aa36a1274cd80d4
Henrik Lissner <henrik@lissner.net>
2019-11-19 02:56:07 -0500

    transmission: whitelist local network

--- modules/services/plex.nix
+++ modules/services/plex.nix
@@ -11,6 +11,9 @@
     openFirewall = true;
   };
 
-  services.transmission.enable = true;
+  services.transmission = {
+    enable = true;
+    settings.rpc-whitelist = "127.0.0.1,192.168.*.*";
+  };
   networking.firewall.allowedTCPPorts = [ 9091 ];
 }
