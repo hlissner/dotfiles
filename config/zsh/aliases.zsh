b58381faf09302c6257788cc3f6d5ae21eadc52a
Henrik Lissner <henrik@lissner.net>
2019-11-26 17:04:55 -0500

    Refactor zsh aliases

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -5,13 +5,12 @@ alias mv='mv -i'
 
 alias mkdir='mkdir -p'
 alias wget='wget -c'
+alias rg='noglob rg'
+alias bc='bc -lq'
 
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
-
-alias rg='noglob rg'
-alias ag='noglob ag -p $HOME/.config/ag/agignore'
 
 
 ### convenience
@@ -24,12 +23,17 @@ alias q=exit
 alias clr=clear
 alias sudo='sudo '
 
-alias exa='exa --group-directories-first'
-alias l='exa -1'
-alias ll='exa -l'
-alias la='LC_COLLATE=C exa -la'
-alias mk=make
+if command -v exa >/dev/null; then
+  alias exa='exa --group-directories-first'
+  alias l='exa -1'
+  alias ll='exa -l'
+  alias la='LC_COLLATE=C exa -la'
+else
+  alias l='ls -1'
+  alias ll='ls -l'
+fi
 
+alias mk=make
 alias rcp='rsync -vaP --delete'
 alias rmirror='rsync -rtvu --delete'
 alias gurl='curl --compressed'
