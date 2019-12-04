63307a9866ea9ca6942f2bcfcd1dc4236bcf0928
Henrik Lissner <henrik@lissner.net>
2019-12-03 20:33:54 -0500

    Install lorri from nixpkgs-unstable

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -1,21 +1,8 @@
 { config, lib, pkgs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      lorri =
-        let src = (super.fetchFromGitHub {
-              owner = "target";
-              repo = "lorri";
-              rev = "38eae3d487526ece9d1b8c9bb0d27fb45cf60816";
-              sha256 = "11k9lxg9cv6dlxj4haydvw4dhcfyszwvx7jx9p24jadqsy9jmbj4";
-            });
-        in import src { inherit src; };
-    })
-  ];
-
   environment.systemPackages = with pkgs; [
     direnv
-    lorri
+    (import <nixpkgs-unstable> {}).lorri
   ];
 }
