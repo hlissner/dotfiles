92ede195f70f74c88a9a319dcdd9404ebfa9d1fa
Henrik Lissner <henrik@lissner.net>
2020-01-03 06:48:04 -0500

    Add recording.nix module
    And remove kdenlive & obs modules. Moves obs to recording module.
    
    I've replaced kdenlive with DaVinci Resolve (can't believe it's free!)

---
+++ modules/recording.nix
@@ -0,0 +1,19 @@
+# I use OBS to capture footage/stream and DaVinci Resolve for editing (on my
+# Windows system -- it will run on windows, but there is no way to automate its
+# installation, what with their devs regsitration-gating their free download).
+#
+# I also use ffmpeg for small casts/gifs, but this module is for the heavy duty
+# stuff.
+#
+# This module is best paired with ./audio-editing.nix
+
+{ config, lib, pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    slop       # for selecting arbitrary regions on screen
+    ffmpeg     # for recording gifs/mp4s quickly
+    audacity   # for recording and remastering audio
+    obs-studio # for streaming to twitch/recording for youtube
+    handbrake  # for encoding footage
+  ];
+}
