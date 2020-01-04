8f7f5a5cce28ee9027ba8d6133ec2f96214acf87
Henrik Lissner <henrik@lissner.net>
2020-01-04 00:14:19 -0500

    Expand & refactor editor/emacs module

--- modules/dev/rust.nix
+++ modules/dev/rust.nix
@@ -3,7 +3,6 @@
 {
   environment.systemPackages = with pkgs; [
     rustup
-    rls
   ];
 
   home.xdg.configFile = {
--- modules/editors/emacs.nix
+++ modules/editors/emacs.nix
@@ -1,21 +1,34 @@
-{ config, lib, pkgs, ... }:
+# Emacs is my main driver. I'm the author of Doom Emacs
+# <https://github.com/hlissner/doom-emacs>. This module sets it up to meed my
+# particular Doomy needs.
 
+{ config, lib, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    (lib.mkIf (config.programs.gnupg.agent.enable) pinentry_emacs)
-
-    zstd
-    editorconfig-core-c
-    (ripgrep.override {withPCRE2 = true;})
-    # Doom Emacs + dependencies
+    # Doom dependencies
     emacs
+    git
+    (ripgrep.override {withPCRE2 = true;})
+    # Optional dependencies
+    editorconfig-core-c # per-project style config
+    fd                  # faster projectile indexing
+    imagemagick         # for image-dired
+    gnutls              # for TLS connectivity
+    (lib.mkIf (config.programs.gnupg.agent.enable)
+      pinentry_emacs)   # in-emacs gnupg prompts
+    zstd                # for undo-tree compression
+    ffmpeg              # for screen recording emacs
+    # Module dependencies
     sqlite                          # :tools (lookup +docsets)
     texlive.combined.scheme-medium  # :lang org -- for latex previews
     ccls                            # :lang (cc +lsp)
     nodePackages.javascript-typescript-langserver # :lang (javascript +lsp)
+    rls                             # :lang (rust +lsp)
   ];
 
-  fonts.fonts = [ pkgs.emacs-all-the-icons-fonts ];
+  fonts.fonts = [
+    pkgs.emacs-all-the-icons-fonts
+  ];
 
   home.xdg.configFile = {
     "zsh/rc.d/aliases.emacs.zsh".source = <config/emacs/aliases.zsh>;
