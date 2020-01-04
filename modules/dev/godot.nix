985cec63c76d190143341b16dd372a9ef909085d
Henrik Lissner <henrik@lissner.net>
2020-01-04 00:14:32 -0500

    Add godot module

---
+++ modules/dev/godot.nix
@@ -0,0 +1,10 @@
+# I gamedev as a hobby. For making games, I mainly use C++ or Rust (and
+# occasionally Lua). However, when I need to prototype an idea, I reach for
+# godot (or JS, if I want it to be playable for others).
+
+{ config, lib, pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    godot
+  ];
+}
