690a2c285227bce410428bb7628ed0b0c338fc6f
Henrik Lissner <henrik@lissner.net>
2020-01-13 02:11:28 -0500

    Disable tablet configuration
    Should use udev for these

--- machines/kuro.nix
+++ machines/kuro.nix
@@ -45,12 +45,14 @@
   my.alias.teensyload = "sudo teensy-loader-cli -w -v --mcu=atmega32u4";
   # For my intuos4 pro. My cintiq doesn't work on Linux though :(
   services.xserver.wacom.enable = true;
-  my.init = ''
-    # lock tablet to main display
-    if xinput list --id-only "Wacom Intuos Pro S Pen stylus"; then
-      xinput map-to-output $(xinput list --id-only "Wacom Intuos Pro S Pen stylus") DVI-I-1
-      xinput map-to-output $(xinput list --id-only "Wacom Intuos Pro S Pen eraser") DVI-I-1
-      xinput map-to-output $(xinput list --id-only "Wacom Intuos Pro S Pen cursor") DVI-I-1
-    fi
-  '';
+
+  # TODO Move this to udev
+  # my.init = ''
+  #   # lock tablet to main display
+  #   if xinput list --id-only "Wacom Intuos Pro S Pen stylus" 2>&1 >/dev/null; then
+  #     xinput map-to-output $(xinput list --id-only "Wacom Intuos Pro S Pen stylus") DVI-I-1
+  #     xinput map-to-output $(xinput list --id-only "Wacom Intuos Pro S Pen eraser") DVI-I-1
+  #     xinput map-to-output $(xinput list --id-only "Wacom Intuos Pro S Pen cursor") DVI-I-1
+  #   fi
+  # '';
 }
