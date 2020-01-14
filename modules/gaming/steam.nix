5e3ce74ad3f7d8efdbfc325130bc9536b6a38d78
Henrik Lissner <henrik@lissner.net>
2020-01-14 00:26:00 -0500

    Add xbox controller drivers

--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -4,6 +4,7 @@
   my.packages = with pkgs; [
     steam
     steam-run-native  # for GOG or humblebundle games
+    xboxdrv  # driver for 360 controller
   ];
 
   hardware.opengl.driSupport32Bit = true;
