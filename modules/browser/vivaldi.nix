26f9c85a8b19115a4cac0ad1a5f779c26f698754
Henrik Lissner <henrik@lissner.net>
2020-01-17 19:36:39 -0500

    Add vivaldi-private script/desktop item

--- modules/browser/vivaldi.nix
+++ modules/browser/vivaldi.nix
@@ -3,6 +3,18 @@
 {
   my.packages = with pkgs; [
     vivaldi
+    (pkgs.writeScriptBin "vivaldi-private" ''
+      #!${stdenv.shell}
+      ${vivaldi}/bin/vivaldi --incognito "$@"
+    '')
+    (makeDesktopItem {
+      name = "vivaldi-private";
+      desktopName = "Vivaldi (Incognito)";
+      genericName = "Open an incognito Vivaldi window";
+      icon = "vivaldi";
+      exec = "${vivaldi}/bin/vivaldi --incognito";
+      categories = "Network";
+    })
   ];
   my.env.BROWSER = "vivaldi";
 }
