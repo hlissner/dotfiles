6675d7ba8898727624907d39bda77f43344f80aa
Henrik Lissner <henrik@lissner.net>
2020-04-22 20:41:34 -0400

    Refactor emacs.nix; install a little more of texlive

--- modules/editors/emacs.nix
+++ modules/editors/emacs.nix
@@ -19,15 +19,14 @@ with lib;
         emacsUnstable
         git
         (ripgrep.override {withPCRE2 = true;})
+        gnutls              # for TLS connectivity
 
         ## Optional dependencies
-        editorconfig-core-c # per-project style config
         fd                  # faster projectile indexing
-        gnutls              # for TLS connectivity
         imagemagick         # for image-dired
         (lib.mkIf (config.programs.gnupg.agent.enable)
           pinentry_emacs)   # in-emacs gnupg prompts
-        zstd                # for undo-tree compression
+        zstd                # for undo-fu-session/undo-tree compression
 
         ## Module dependencies
         # :checkers spell
@@ -37,14 +36,16 @@ with lib;
         aspellDicts.en-science
         # :checkers grammar
         languagetool
-        # :tools lookup
+        # :tools editorconfig
+        editorconfig-core-c # per-project style config
+        # :tools lookup & :lang org +roam
         sqlite
         # :lang cc
         ccls
         # :lang javascript
         nodePackages.javascript-typescript-langserver
         # :lang latex & :lang org (latex previews)
-        texlive.combined.scheme-medium
+        texlive.combined.scheme-tetex
         # :lang rust
         rustfmt
         rls
