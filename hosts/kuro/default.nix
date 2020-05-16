75c928e00e41bb6d656a34336743afb644563bd6
Henrik Lissner <henrik@lissner.net>
2020-05-15 20:56:59 -0400

    Enable node module on kuro

--- hosts/kuro/default.nix
+++ hosts/kuro/default.nix
@@ -41,6 +41,7 @@
       cc.enable = true;
       common-lisp.enable = true;
       rust.enable = true;
+      node.enable = true;
       # lua.enable = true;
       # lua.love2d.enable = true;
     };
