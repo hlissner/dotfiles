c9efc8c9e074bc21448e557ffa510b8659203133
Henrik Lissner <henrik@lissner.net>
2020-12-18 23:42:52 -0500

    Add basic bitwarden service module

---
+++ modules/services/bitwarden.nix
@@ -0,0 +1,16 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.services.bitwarden;
+in {
+  options.modules.services.bitwarden = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    services.bitwarden_rs = {
+      enable = true;
+    };
+  };
+}
