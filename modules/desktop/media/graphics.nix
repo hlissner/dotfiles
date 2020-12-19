9d257c8e55ec58cbbfbcd6d20c8083e91ab126f1
Henrik Lissner <henrik@lissner.net>
2020-12-19 04:31:58 -0500

    Bump

--- modules/desktop/media/graphics.nix
+++ modules/desktop/media/graphics.nix
@@ -35,7 +35,7 @@ in {
       (if cfg.raster.enable then [
         krita
         gimp
-        gimpPlugins.resynthesizer2  # content-aware scaling in gimp
+        gimpPlugins.resynthesizer  # content-aware scaling in gimp
       ] else []) ++
 
       # Sprite sheets & animation
