c376499be88771dd99aec55408451db9528c916c
Henrik Lissner <henrik@lissner.net>
2020-12-19 02:48:23 -0500

    hosts/ao: update hardware config
    No longer has an external drive (now that I have a NAS).
    
    And broadcom config isn't needed anyway (I never use wifi with this
    system).

--- hosts/ao/hardware-configuration.nix
+++ hosts/ao/hardware-configuration.nix
@@ -6,7 +6,7 @@
     initrd.availableKernelModules = [ "ohci_pci" "ehci_pci" "ahci" "firewire_ohci" "usb_storage" "usbhid" "sd_mod" "sr_mod" "sdhci_pci" ];
     initrd.kernelModules = [ ];
     kernelModules = [ "kvm-intel" "wl" ];
-    extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+    extraModulePackages = [];
   };
 
   ## CPU
@@ -22,11 +22,6 @@
     "/boot" = {
       device = "/dev/disk/by-label/boot";
       fsType = "vfat";
-    };
-    "/mnt/data" = {
-      device = "/dev/disk/by-label/data";
-      fsType = "ext4";
-      options = [ "noatime" ];
     };
   };
   swapDevices = [{ device = "/dev/disk/by-label/swap"; }];
