608b3472b93e46f429531bcc639cd0f8a8b761cc
Henrik Lissner <henrik@lissner.net>
2021-01-02 00:24:20 -0500

    security.acme.acceptTerms = true
    So we don't have to later.

--- modules/security.nix
+++ modules/security.nix
@@ -13,4 +13,7 @@
   # Change me later!
   user.initialPassword = "nixos";
   users.users.root.initialPassword = "nixos";
+
+  # So we don't have to do this later...
+  security.acme.acceptTerms = true;
 }
