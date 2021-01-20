be343218e98e5d0434310c0aad146eba7c57d953
Henrik Lissner <henrik@lissner.net>
2021-01-20 03:05:55 -0500

    Add modules/desktop/browsers/brave.nix

---
+++ modules/desktop/browsers/brave.nix
@@ -0,0 +1,28 @@
+# modules/browser/brave.nix --- https://publishers.basicattentiontoken.org
+#
+# A FOSS and privacy-minded browser.
+
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.browsers.brave;
+in {
+  options.modules.desktop.browsers.brave = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      brave
+      (makeDesktopItem {
+        name = "brave-private";
+        desktopName = "Brave Web Browser";
+        genericName = "Open a private Brave window";
+        icon = "brave";
+        exec = "${brave}/bin/brave --incognito";
+        categories = "Network";
+      })
+    ];
+  };
+}
