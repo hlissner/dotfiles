a1c46f27ce7350389a9dd0b678dc01fb4779e91b
Henrik Lissner <henrik@lissner.net>
2021-01-24 13:05:26 -0500

    Update shell.nix to reflect recent changes

--- shell.nix
+++ shell.nix
@@ -3,16 +3,15 @@
 with pkgs;
 let nixBin =
       writeShellScriptBin "nix" ''
-          ${nixFlakes}/bin/nix --option experimental-features "nix-command flakes" "$@"
+        ${nixFlakes}/bin/nix --option experimental-features "nix-command flakes" "$@"
       '';
 in mkShell {
   buildInputs = [
     git
-    gnupg
     nix-zsh-completions
   ];
   shellHook = ''
-    export DOTFILES="$(pwd)"
-    export PATH="$DOTFILES/bin:${nixBin}/bin:$PATH"
+    export FLAKE="$(pwd)"
+    export PATH="$FLAKE/bin:${nixBin}/bin:$PATH"
   '';
 }
