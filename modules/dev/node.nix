60e3b114db2838284a2c1635224f7bd038bd1195
Henrik Lissner <henrik@lissner.net>
2021-02-27 11:20:16 -0500

    Resolve paths to yarn/npm

--- modules/dev/node.nix
+++ modules/dev/node.nix
@@ -24,11 +24,11 @@ in {
     env.NPM_CONFIG_TMP        = "$XDG_RUNTIME_DIR/npm";
     env.NPM_CONFIG_PREFIX     = "$XDG_CACHE_HOME/npm";
     env.NODE_REPL_HISTORY     = "$XDG_CACHE_HOME/node/repl_history";
-    env.PATH = [ "$(yarn global bin)" ];
+    env.PATH = [ "$(${pkgs.yarn}/bin/yarn global bin)" ];
 
     # Run locally installed bin-script, e.g. n coffee file.coffee
     environment.shellAliases = {
-      n  = "PATH=\"$(npm bin):$PATH\"";
+      n  = "PATH=\"$(${pkgs.nodejs}/bin/npm bin):$PATH\"";
       ya = "yarn";
     };
 
