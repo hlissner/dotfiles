699e272643732776a82b14e2fe4ebdb923c894cb
Henrik Lissner <henrik@lissner.net>
2021-02-27 23:03:59 -0500

    hosts/ao: refactor nginx config

--- hosts/ao/default.nix
+++ hosts/ao/default.nix
@@ -37,21 +37,22 @@ with lib.my;
     default = true;
     forceSSL = true;
     enableACME = true;
-    locations."~* \.(?:ico|css|map|js|gif|jpe?g|png|ttf|woff)$".extraConfig = ''
-      access_log off;
-      expires 30d;
-      add_header Pragma public;
-      add_header Cache-Control "public, mustrevalidate, proxy-revalidate";
-      proxy_pass https://kiiro:8001;
-      proxy_buffering off;
-    '';
-    locations."/".extraConfig = ''
-      client_max_body_size 10m;
-      proxy_ssl_server_name on;
-      proxy_pass_header Authorization;
-      proxy_pass https://kiiro:8001;
-      proxy_buffering off;
-    '';
+    locations = {
+      "~* \.(?:ico|css|map|js|gif|jpe?g|png|ttf|woff)$".extraConfig = ''
+        access_log off;
+        expires 30d;
+        add_header Pragma public;
+        add_header Cache-Control "public, mustrevalidate, proxy-revalidate";
+        proxy_pass http://kiiro:8000;
+        proxy_buffering off;
+      '';
+      "/".extraConfig = ''
+        client_max_body_size 10m;
+        proxy_pass http://kiiro:8000;
+        proxy_buffering off;
+        proxy_redirect off;
+      '';
+    };
   };
 
   security.acme.email = "henrik@lissner.net";
