51d63a1829ac87dbe90226e679afe3af7c915947
Henrik Lissner <henrik@lissner.net>
2021-03-11 23:21:57 -0500

    Bump
    * Updated agenix: github:ryantm/agenix/ed7e69bff3c17671349839f696e88e345eb9eb24 -> github:ryantm/agenix/9eb981eeb552e0856cb562b7643b311933a0e992
    * Updated emacs-overlay: github:nix-community/emacs-overlay/063ee066b5095dd2f21f47c582b04024674974a8 -> github:nix-community/emacs-overlay/daa4b15755f6cccb462efed696abceee0994bb96
    * Updated home-manager: github:rycee/home-manager/f45c7000d544f17e5499e2ae6b505b5f705c5d61 -> github:rycee/home-manager/040ea28e448a93d24540b7cf2eda4b25300c5ab1
    * Updated nixos-hardware: github:nixos/nixos-hardware/c9b965a43e582765f797f8b75d50cb2874b30f30 -> github:nixos/nixos-hardware/831de9cac9ff2ea8cf4c3742020ea92e82ac8ac3
    * Updated nixpkgs: github:NixOS/nixpkgs/ff96a0fa5635770390b184ae74debea75c3fd534 -> github:NixOS/nixpkgs/29b0d4d0b600f8f5dd0b86e3362a33d4181938f9
    * Updated nixpkgs-unstable: github:NixOS/nixpkgs/07b0c7bc4104b87cb5223ec10e32b863d10768ad -> github:NixOS/nixpkgs/d02b1cc8ed29b6967119fd95267ee1a955015d59
    
    Also, removed noexec from /tmp because it screws with build processes
    that deploy/execute scripts to/out of /tmp, causing permission errors
    while building.

--- modules/security.nix
+++ modules/security.nix
@@ -3,20 +3,14 @@
 {
   ## System security tweaks
   # sets hidepid=2 on /proc (make process info visible only to owning user)
-  security.hideProcessInformation = true;
+  # NOTE Was removed on nixpkgs-unstable because it doesn't do anything
+  # security.hideProcessInformation = true;
   # Prevent replacing the running kernel w/o reboot
   security.protectKernelImage = true;
 
   # tmpfs = /tmp is mounted in ram. Doing so makes temp file management speedy
   # on ssd systems, and volatile! Because it's wiped on reboot.
   boot.tmpOnTmpfs = lib.mkDefault true;
-  systemd.mounts = lib.mkIf config.boot.tmpOnTmpfs [{
-    what = "tmpfs";
-    where = "/tmp";
-    type = "tmpfs";
-    # Added noexec (for added security) and reduce size from 50% to 25%
-    options = "mode=1777,strictatime,rw,nosuid,nodev,noexec,size=25%";
-  }];
   # If not using tmpfs, which is naturally purged on reboot, we must clean it
   # /tmp ourselves. /tmp should be volatile storage!
   boot.cleanTmpDir = lib.mkDefault (!config.boot.tmpOnTmpfs);
