7fa949717b9f225212f8f70159d85f70394c38cb
Henrik Lissner <henrik@lissner.net>
2021-04-13 20:25:38 -0400

    Enable docker module

--- hosts/kuro/default.nix
+++ hosts/kuro/default.nix
@@ -58,6 +58,7 @@
     };
     services = {
       ssh.enable = true;
+      docker.enable = true;
       # Needed occasionally to help the parental units with PC problems
       # teamviewer.enable = true;
     };
