79acd090f43421a48b5b5001a47aa4eb8d719e7b
Henrik Lissner <henrik@lissner.net>
2021-09-03 23:17:07 +0200

    feat: add hosts/aijiro config
    My portable desktop machine.

---
+++ hosts/aijiro/default.nix
@@ -0,0 +1,86 @@
+# Shiro -- my laptop
+
+{ ... }:
+{
+  imports = [
+    ../home.nix
+    ./hardware-configuration.nix
+  ];
+
+  ## Modules
+  modules = {
+    desktop = {
+      bspwm.enable = true;
+      apps = {
+        discord.enable = true;
+        rofi.enable = true;
+        # godot.enable = true;
+      };
+      browsers = {
+        default = "brave";
+        brave.enable = true;
+        # firefox.enable = true;
+        # qutebrowser.enable = true;
+      };
+      gaming = {
+        # steam.enable = true;
+        # emulators.enable = true;
+        # emulators.psx.enable = true;
+      };
+      media = {
+        daw.enable = true;
+        documents.enable = true;
+        graphics.enable = true;
+        mpv.enable = true;
+        recording.enable = true;
+        spotify.enable = true;
+      };
+      term = {
+        default = "xst";
+        st.enable = true;
+      };
+      vm = {
+        qemu.enable = true;
+      };
+    };
+    dev = {
+      node.enable = true;
+      rust.enable = true;
+      python.enable = true;
+    };
+    editors = {
+      default = "nvim";
+      emacs.enable = true;
+      vim.enable = true;
+    };
+    shell = {
+      # adl.enable = true;
+      vaultwarden.enable = true;
+      direnv.enable = true;
+      git.enable    = true;
+      gnupg.enable  = true;
+      tmux.enable   = true;
+      zsh.enable    = true;
+    };
+    services = {
+      ssh.enable = true;
+      docker.enable = true;
+      # Needed occasionally to help the parental units with PC problems
+      # teamviewer.enable = true;
+    };
+    theme.active = "alucard";
+  };
+
+
+  ## Local config
+  programs.ssh.startAgent = true;
+  services.openssh.startWhenNeeded = true;
+
+  networking.networkmanager.enable = true;
+  # The global useDHCP flag is deprecated, therefore explicitly set to false
+  # here. Per-interface useDHCP will be mandatory in the future, so this
+  # generated config replicates the default behaviour.
+  networking.useDHCP = false;
+
+  time.timeZone = "Europe/Copenhagen";
+}
