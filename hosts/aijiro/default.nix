e8cd6f02d4f83ba310c5159201f2fc2d6ca24c5b
Henrik Lissner <henrik@lissner.net>
2021-09-04 07:49:55 +0200

    fix(aijiro): enable wireless

--- hosts/aijiro/default.nix
+++ hosts/aijiro/default.nix
@@ -76,7 +76,7 @@
   programs.ssh.startAgent = true;
   services.openssh.startWhenNeeded = true;
 
-  networking.networkmanager.enable = true;
+  networking.wireless.enable = true;
   # The global useDHCP flag is deprecated, therefore explicitly set to false
   # here. Per-interface useDHCP will be mandatory in the future, so this
   # generated config replicates the default behaviour.
