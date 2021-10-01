460d834b228f55bba4015fb78b544f3189375811
Henrik Lissner <henrik@lissner.net>
2021-10-01 23:02:02 +0200

    feat(nginx): add optional cloudflare support

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -6,41 +6,56 @@ let cfg = config.modules.services.nginx;
 in {
   options.modules.services.nginx = {
     enable = mkBoolOpt false;
+    enableCloudflareSupport = mkBoolOpt false;
   };
 
-  config = mkIf cfg.enable {
-    networking.firewall.allowedTCPPorts = [ 80 443 ];
+  config = mkMerge [
+    (mkIf cfg.enable {
+      networking.firewall.allowedTCPPorts = [ 80 443 ];
 
-    user.extraGroups = [ "nginx" ];
+      user.extraGroups = [ "nginx" ];
 
-    services.nginx = {
-      enable = true;
+      services.nginx = {
+        enable = true;
 
-      # Use recommended settings
-      recommendedGzipSettings = true;
-      recommendedOptimisation = true;
-      recommendedProxySettings = true;
-      recommendedTlsSettings = true;
+        # Use recommended settings
+        recommendedGzipSettings = true;
+        recommendedOptimisation = true;
+        recommendedProxySettings = true;
+        recommendedTlsSettings = true;
 
-      # Reduce the permitted size of client requests, to reduce the likelihood
-      # of buffer overflow attacks. This can be tweaked on a per-vhost basis, as
-      # needed.
-      clientMaxBodySize = "128k";  # default 10m
-      commonHttpConfig = ''
-        client_body_buffer_size  4k;       # default: 8k
-        large_client_header_buffers 2 4k;  # default: 4 8k
+        # Reduce the permitted size of client requests, to reduce the likelihood
+        # of buffer overflow attacks. This can be tweaked on a per-vhost basis, as
+        # needed.
+        clientMaxBodySize = "128k";  # default 10m
+        commonHttpConfig = ''
+          client_body_buffer_size  4k;       # default: 8k
+          large_client_header_buffers 2 4k;  # default: 4 8k
 
-        map $sent_http_content_type $expires {
-            default                    off;
-            text/html                  10m;
-            text/css                   max;
-            application/javascript     max;
-            application/pdf            max;
-            ~image/                    max;
-        }
+          map $sent_http_content_type $expires {
+              default                    off;
+              text/html                  10m;
+              text/css                   max;
+              application/javascript     max;
+              application/pdf            max;
+              ~image/                    max;
+          }
+        '';
+      };
+    })
+
+    (mkIf cfg.enableCloudflareSupport {
+      services.nginx.commonHttpConfig = ''
+        ${concatMapStrings (ip: "set_real_ip_from ${ip};\n")
+          (filter (line: line != "")
+            (splitString "\n" ''
+              ${readFile (fetchurl "https://www.cloudflare.com/ips-v4/")}
+              ${readFile (fetchurl "https://www.cloudflare.com/ips-v6/")}
+            ''))}
+        real_ip_header CF-Connectting-IP;
       '';
-    };
-  };
+    })
+  ];
 }
 
 # Helpful nginx snippets
