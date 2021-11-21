01849b8290711a7f8a15b777524def300593205d
Henrik Lissner <git@henrik.io>
2021-11-21 03:26:00 +0100

    refactor!(zsh): reorganize zsh sources
    BREAKING CHANGE: My dotfiles no longer look for ~/.zshenv and ~/.zshrc
    for local config. Instead, it looks for ~/.config/zsh/local.zsh{env,rc}.

--- config/zsh/config.zsh
+++ config/zsh/config.zsh
@@ -6,10 +6,22 @@ if [[ $TERM == dumb || -n $INSIDE_EMACS ]]; then
   PS1='$ '
 fi
 
-## General
+## Plugins
+# fasd
 export _FASD_DATA="$XDG_CACHE_HOME/fasd"
 export _FASD_VIMINFO="$XDG_CACHE_HOME/viminfo"
+_cache fasd --init posix-alias zsh-{hook,{c,w}comp{,-install}}
 
+# fzf
+if (( $+commands[fd] )); then
+  export FZF_DEFAULT_OPTS="--reverse --ansi"
+  export FZF_DEFAULT_COMMAND="fd ."
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_ALT_C_COMMAND="fd -t d . $HOME"
+fi
+
+
+## ZSH configuration
 # Treat these characters as part of a word.
 WORDCHARS='_-*?[]~&.;!#$%^(){}<>'
 
