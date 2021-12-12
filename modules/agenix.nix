4539d607778820cd6fd97b6c81c1cfcd6cb7e226
Henrik Lissner <git@henrik.io>
2021-12-12 19:55:01 +0100

    bump
    • Updated input 'agenix':
        'github:ryantm/agenix/a0e9ca505c82e762d39e9477a428b537a0aab022' (2021-11-23)
      → 'github:ryantm/agenix/57806bf7e340f4cae705c91748d4fdf8519293a9' (2021-12-07)
    • Updated input 'emacs-overlay':
        'github:nix-community/emacs-overlay/df5519990439ca5e65e72107d5ed83b846e53f6a' (2021-11-24)
      → 'github:nix-community/emacs-overlay/b5754823ce922a10a73dc534a350d53ee134be84' (2021-12-12)
    • Updated input 'home-manager':
        'github:rycee/home-manager/df931a59a5864d6ff0c5d83598135816f8593647' (2021-11-24)
      → 'github:rycee/home-manager/3d46c011d2cc2c9ca24d9b803e9daf156d9429ea' (2021-12-12)
    • Updated input 'nixos-hardware':
        'github:nixos/nixos-hardware/08cda8e3a5a4e685af525e5a589dfeb74267d505' (2021-11-22)
      → 'github:nixos/nixos-hardware/2a7063461c3751d83869a2a0a8ebc59e34bec5b2' (2021-12-11)
    • Updated input 'nixpkgs-unstable':
        'github:NixOS/nixpkgs/98747f27ecfee70c8c97b195cbb94df80a074dda' (2021-11-22)
      → 'github:NixOS/nixpkgs/e6df26a654b7fdd59a068c57001eab5736b1363c' (2021-12-11)

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -20,8 +20,8 @@ in {
         owner = mkDefault config.user.name;
       }) (import secretsFile)
       else {};
-    sshKeyPaths =
-      options.age.sshKeyPaths.default ++ (filter pathExists [
+    identityPaths =
+      options.age.identityPaths.default ++ (filter pathExists [
         "${config.user.home}/.ssh/id_ed25519"
         "${config.user.home}/.ssh/id_rsa"
       ]);
