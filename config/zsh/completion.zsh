c106f5cc311bd5534ca08140c566e6240d1f1a6c
Henrik Lissner <git@henrik.io>
2021-12-18 19:10:51 +0100

    tweak(zsh): polish completion & history settings
    More history and better completion behavior (plus auto-correcting
    typos).

--- config/zsh/completion.zsh
+++ config/zsh/completion.zsh
@@ -25,6 +25,14 @@ unsetopt COMPLETE_ALIASES  # Completion for aliases
 # unsetopt ALWAYS_TO_END     # Move cursor to the end of a completed word.
 unsetopt CASE_GLOB
 
+# Fuzzy match mistyped completions.
+zstyle ':completion:*' completer _complete _list _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+# Increase the number of errors based on the length of the typed word.
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
+# Don't complete unavailable commands.
+zstyle ':completion:*:functions' ignored-patterns '(_*|.*|pre(cmd|exec))'
 # Group matches and describe.
 zstyle ':completion:*:corrections' format '%B%F{green}%d (errors: %e)%f%b'
 zstyle ':completion:*:messages' format '%B%F{yellow}%d%f%b'
@@ -32,26 +40,26 @@ zstyle ':completion:*:warnings' format '%B%F{red}No such %d%f%b'
 zstyle ':completion:*:errors' format '%B%F{red}No such %d%f%b'
 zstyle ':completion:*:descriptions' format $'%{\e[35;1m%}%d%{\e[0m%}'
 zstyle ':completion:*:default' list-prompt '%S%M matches%s'
-
-# Don't complete unavailable commands.
-zstyle ':completion:*:functions' ignored-patterns '(_*|.*|pre(cmd|exec))'
-
-# History
+# Omit parent and current directories from completion results when they are
+# already named in the input.
+zstyle ':completion:*:*:cd:*' ignore-parents parent pwd
+# Merge multiple, consecutive slashes in paths
+zstyle ':completion:*' squeeze-slashes true
+# Don't wrap around when navigating to either end of history
 zstyle ':completion:*:history-words' stop yes
 zstyle ':completion:*:history-words' remove-all-dups yes
 zstyle ':completion:*:history-words' list false
 zstyle ':completion:*:history-words' menu yes
-
-# Environmental Variables
+# Exclude internal/fake envvars
 zstyle ':completion::*:(-command-|export):*' fake-parameters ${${${_comps[(I)-value-*]#*,}%%,*}:#-*-}
-
-# Populate hostname completion.
+# Sory array completion candidates
+zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
+# Complete hostnames from ssh files too
 zstyle -e ':completion:*:hosts' hosts 'reply=(
   ${=${=${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) 2>/dev/null)"}%%[#| ]*}//\]:[0-9]*/ }//,/ }//\[/ }
   ${=${${${${(@M)${(f)"$(cat ~/.ssh/config 2>/dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
 )'
-
-# Don't complete uninteresting users...
+# Don't complete uninteresting users
 zstyle ':completion:*:users' ignored-patterns \
   adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
   dbus distcache dovecot fax ftp games gdm gkrellmd gopher \
@@ -60,31 +68,25 @@ zstyle ':completion:*:users' ignored-patterns \
   named netdump news nfsnobody nobody 'nixbld*' nscd ntp nut nx openvpn \
   operator pcap postfix postgres privoxy pulse pvm quagga radvd \
   rpc rpcuser rpm shutdown squid sshd sync 'systemd-*' uucp vcsa xfs '_*'
-
 # ... unless we really want to.
 zstyle '*' single-ignored show
-
 # Ignore multiple entries.
 zstyle ':completion:*:(rm|kill|diff):*' ignore-line other
 zstyle ':completion:*:rm:*' file-patterns '*:all-files'
-
-# Kill
+# PID completion for kill
 zstyle ':completion:*:*:*:*:processes' command 'ps -u $LOGNAME -o pid,user,command -w'
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;36=0=01'
 zstyle ':completion:*:*:kill:*' menu yes select
 zstyle ':completion:*:*:kill:*' force-list always
 zstyle ':completion:*:*:kill:*' insert-ids single
-
-# # Man
-# zstyle ':completion:*:manuals' separate-sections true
-# zstyle ':completion:*:manuals.(^1*)' insert-sections true
-
+# Man
+zstyle ':completion:*:manuals' separate-sections true
+zstyle ':completion:*:manuals.(^1*)' insert-sections true
 # Media Players
 zstyle ':completion:*:*:mpg123:*' file-patterns '*.(mp3|MP3):mp3\ files *(-/):directories'
 zstyle ':completion:*:*:mpg321:*' file-patterns '*.(mp3|MP3):mp3\ files *(-/):directories'
 zstyle ':completion:*:*:ogg123:*' file-patterns '*.(ogg|OGG|flac):ogg\ files *(-/):directories'
 zstyle ':completion:*:*:mocp:*' file-patterns '*.(wav|WAV|mp3|MP3|ogg|OGG|flac):ogg\ files *(-/):directories'
-
 # SSH/SCP/RSYNC
 zstyle ':completion:*:(scp|rsync):*' tag-order 'hosts:-host:host hosts:-domain:domain hosts:-ipaddr:ip\ address *'
 zstyle ':completion:*:(scp|rsync):*' group-order users files all-files hosts-domain hosts-host hosts-ipaddr
--- config/zsh/config.zsh
+++ config/zsh/config.zsh
@@ -52,26 +52,27 @@ unsetopt CHECK_JOBS       # Don't report on jobs when shell exit.
 
 ## History
 HISTFILE="$XDG_CACHE_HOME/zhistory"
-HISTSIZE=1000                    # Max events to store in internal history.
-SAVEHIST=1000                    # Max events to store in history file.
+HISTSIZE=100000   # Max events to store in internal history.
+SAVEHIST=100000   # Max events to store in history file.
 
 setopt BANG_HIST                 # Don't treat '!' specially during expansion.
-setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
+setopt EXTENDED_HISTORY          # Include start time in history records
 setopt APPEND_HISTORY            # Appends history to history file on exit
 setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
 setopt SHARE_HISTORY             # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
 setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
-setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
+setopt HIST_IGNORE_ALL_DUPS      # Remove old events if new event is a duplicate
 setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
 setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
 setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
+setopt HIST_REDUCE_BLANKS        # Minimize unnecessary whitespace
 setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
 setopt HIST_BEEP                 # Beep when accessing non-existent history.
 
 ## Directories
 DIRSTACKSIZE=9
-unsetopt AUTO_CD            # Implicit CD slows down zsh-autocomplete
+unsetopt AUTO_CD            # Implicit CD slows down plugins
 setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
 setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
 setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
