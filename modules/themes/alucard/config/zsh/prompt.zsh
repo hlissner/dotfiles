d429305656125b6f2bcb98ea271f2b5e041312e4
Henrik Lissner <git@henrik.io>
2021-12-19 03:08:52 +0100

    fix(zsh): show 2 hostname components in prompt
    ...on remote systems.

--- modules/themes/alucard/config/zsh/prompt.zsh
+++ modules/themes/alucard/config/zsh/prompt.zsh
@@ -660,11 +660,11 @@
   typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=180
 
   # Context format when running with privileges: bold user@hostname.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%B%n@%m'
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%B%n@%2m'
   # Context format when in SSH without privileges: user@hostname.
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='%n@%m'
+  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='%n@%2m'
   # Default context format (no privileges, no SSH): user@hostname.
-  typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE='%n@%m'
+  typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE='%n@%2m'
 
   # Don't show context unless running with privileges or in SSH.
   # Tip: Remove the next line to always show context.
