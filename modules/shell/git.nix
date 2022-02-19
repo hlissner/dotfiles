06bb1989cf1c656835b096cdcabc65d688086744
Henrik Lissner <git@henrik.io>
2022-02-19 20:37:31 +0100

    feat(git): add git-annex

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -11,6 +11,7 @@ in {
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
+      gitAndTools.git-annex
       gitAndTools.gh
       gitAndTools.git-open
       gitAndTools.diff-so-fancy
