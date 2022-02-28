0b7782d92463a971c3cfab498816549beee5d944
Henrik Lissner <git@henrik.io>
2022-02-28 16:43:53 +0100

    refactor(bluetooth): simplify
    Now that we're on Pipewire, I don't need all this pulseaudio hackery.

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -7,25 +7,9 @@ let hwCfg = config.modules.hardware;
 in {
   options.modules.hardware.bluetooth = {
     enable = mkBoolOpt false;
-    audio.enable = mkBoolOpt false;
   };
 
-  config = mkIf cfg.enable (mkMerge [
-    { hardware.bluetooth.enable = true; }
-
-    (mkIf cfg.audio.enable {
-      hardware.pulseaudio = {
-        # NixOS allows either a lightweight build (default) or full build of
-        # PulseAudio to be installed.  Only the full build has Bluetooth
-        # support, so it must be selected here.
-        package = pkgs.pulseaudioFull;
-        # Enable additional codecs
-        extraModules = [ pkgs.pulseaudio-modules-bt ];
-      };
-
-      hardware.bluetooth.config = {
-        General.Enable = "Source,Sink,Media,Socket";
-      };
-    })
-  ]);
+  config = mkIf cfg.enable {
+    hardware.bluetooth.enable = true;
+  };
 }
