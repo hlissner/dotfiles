63ba9071b92bc66ffe613ea8026a9ef3fb95e531
willb0t <will@damagesinc.net>
2022-06-02 19:09:46 +0000

    add host term

---
+++ hosts/term/default.nix
@@ -0,0 +1,80 @@
+{ pkgs, config, lib, ... }:
+{
+  imports = [
+    ../home.nix
+    ./hardware-configuration.nix
+  ];
+
+  ## Modules
+  modules = {
+    desktop = {
+      bspwm.enable = true;
+      apps = {
+        rofi.enable = true;
+        # godot.enable = true;
+      };
+      browsers = {
+        default = "brave";
+        brave.enable = true;
+        firefox.enable = true;
+        qutebrowser.enable = true;
+      };
+      gaming = {
+        # steam.enable = true;
+        # emulators.enable = true;
+        # emulators.psx.enable = true;
+      };
+      media = {
+        daw.enable = true;
+        documents.enable = true;
+        graphics.enable = true;
+        mpv.enable = true;
+        recording.enable = true;
+        spotify.enable = true;
+      };
+      term = {
+        default = "xst";
+        st.enable = true;
+      };
+      vm = {
+        # qemu.enable = true;
+      };
+    };
+    dev = {
+      node.enable = true;
+      rust.enable = true;
+      # python.enable = true;
+    };
+    editors = {
+      default = "nvim";
+      emacs.enable = true;
+      vim.enable = true;
+    };
+    shell = {
+      adl.enable = true;
+      vaultwarden.enable = true;
+      direnv.enable = true;
+      git.enable    = true;
+      gnupg.enable  = true;
+      tmux.enable   = true;
+      zsh.enable    = true;
+    };
+    services = {
+      ssh.enable = true;
+      # docker.enable = true;
+      # Needed occasionally to help the parental units with PC problems
+      # teamviewer.enable = true;
+    };
+    theme.active = "alucard";
+  };
+
+
+  ## Local config
+  programs.ssh.startAgent = true;
+  services.openssh.startWhenNeeded = true;
+
+  networking.networkmanager.enable = true;
+
+
+  ##END Configure.nix file
+}
