d534beea6a2e33afa073f5e9f5711c1a42e69037
James Stone <jamesmstone@hotmail.com>
2022-06-26 13:58:34 +1000

    upgrade python to 3.8
    as ipython was not compatable with 3.7

--- modules/dev/python.nix
+++ modules/dev/python.nix
@@ -19,13 +19,13 @@ in {
   config = mkMerge [
     (mkIf cfg.enable {
       user.packages = with pkgs; [
-        python37
-        python37Packages.pip
-        python37Packages.ipython
-        python37Packages.black
-        python37Packages.setuptools
-        python37Packages.pylint
-        python37Packages.poetry
+        python38
+        python38Packages.pip
+        python38Packages.ipython
+        python38Packages.black
+        python38Packages.setuptools
+        python38Packages.pylint
+        python38Packages.poetry
       ];
 
       environment.shellAliases = {
