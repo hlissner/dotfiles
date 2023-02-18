6a45a71f1ea60e44189fc169cac2f2aba210eb64
Henrik Lissner <git@henrik.io>
2023-02-17 19:22:58 -0500

    tweak(polybar): redesign
    Instead of an always-on taskbar at the bottom, this redesigns the
    polybar into three on-demand popup status bars that reveal themselves
    when the meta key is down and autohides when it's released.

--- modules/themes/alucard/config/polybar/run.sh
+++ modules/themes/alucard/config/polybar/run.sh
@@ -3,5 +3,14 @@
 pkill -u $UID -x polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main >$XDG_DATA_HOME/polybar.log 2>&1 &
-echo 'Polybar launched...'
+echo 'Launching Polybar...'
+{
+    # Delay so polybar is above other windows
+    sleep 2
+
+    polybardir="$XDG_DATA_HOME/polybar"
+    mkdir -p "$polybardir"
+    polybar main >"$polybardir/main.log" 2>&1 &
+    polybar meta >"$polybardir/meta.log" 2>&1 &
+    polybar date >"$polybardir/date.log" 2>&1 &
+} &
