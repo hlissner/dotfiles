0cebde29d321fb90cbbafa124dc748b2bfff4710
Henrik Lissner <git@henrik.io>
2023-03-21 18:11:48 -0400

    refactor(gnupg): remove tomb; refactor pinentry config
    Modifying the systemd config is the simplest approach.

--- modules/shell/gnupg.nix
+++ modules/shell/gnupg.nix
@@ -12,17 +12,27 @@ in {
   config = mkIf cfg.enable {
     env.GNUPGHOME = "$XDG_CONFIG_HOME/gnupg";
 
-    programs.gnupg.agent.enable = true;
+    systemd.user.services.gpg-agent.serviceConfig.Environment = [
+      "GNUPGHOME=/home/${config.user.name}/.config/gnupg"
+    ];
 
-    user.packages = [ pkgs.tomb ];
+    programs.gnupg.agent = {
+      enable = true;
+      # Don't specify any pinentry flavor in the gpg-agent's service, otherwise
+      # it could potentially overwrite our dotfiles.
+      pinentryFlavor = null;
+    };
 
-    # HACK Without this config file you get "No pinentry program" on 20.03.
-    #      programs.gnupg.agent.pinentryFlavor doesn't appear to work, and this
-    #      is cleaner than overriding the systemd unit.
+    # HACK: Passing GTK2_RC_FILES to the service's Environment didn't work. And
+    #   setting GTK2_RC_FILES globally in
+    #   services.xserver.displayManager.sessionCommands is too nuclear an
+    #   option. This is a clumsy workaround, but is the most targeted.
     home.configFile."gnupg/gpg-agent.conf" = {
       text = ''
         default-cache-ttl ${toString cfg.cacheTTL}
-        pinentry-program ${pkgs.pinentry.gtk2}/bin/pinentry
+        pinentry-program ${pkgs.writeShellScriptBin "pinentry" ''
+          GTK2_RC_FILES=/etc/xdg/gtk-2.0/gtkrc exec ${pkgs.pinentry.gtk2}/bin/pinentry
+        ''}/bin/pinentry
       '';
     };
   };
