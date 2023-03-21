d4f067bd17b871813bb86619ad637723bbe7bf32
Henrik Lissner <git@henrik.io>
2023-03-21 18:08:32 -0400

    perf: add nixos binary cache
    Not sure if strictly necessary, but...

--- default.nix
+++ default.nix
@@ -115,9 +115,11 @@ in {
         registry = mapAttrs (_: v: { flake = v; }) filteredInputs;
         settings = let users = [ "root" config.user.name ]; in {
           substituters = [
+            "https://cache.nixos.org"
             "https://nix-community.cachix.org"
           ];
           trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           ];
           auto-optimise-store = true;
