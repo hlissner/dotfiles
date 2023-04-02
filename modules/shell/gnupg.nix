bafd18a67c4e79303d3c8165ea3a224b0e77514c
Henrik Lissner <git@henrik.io>
2023-04-02 03:31:23 -0400

    fix(gnupg): prioritize gtk2 gui over emacs

--- modules/shell/gnupg.nix
+++ modules/shell/gnupg.nix
@@ -11,29 +11,17 @@ in {
 
   config = mkIf cfg.enable {
     env.GNUPGHOME = "$XDG_CONFIG_HOME/gnupg";
-
     systemd.user.services.gpg-agent.serviceConfig.Environment = [
       "GNUPGHOME=/home/${config.user.name}/.config/gnupg"
     ];
 
     programs.gnupg.agent = {
       enable = true;
-      # Don't specify any pinentry flavor in the gpg-agent's service, otherwise
-      # it could potentially overwrite our dotfiles.
-      pinentryFlavor = null;
+      pinentryFlavor = "gtk2";
     };
 
-    # HACK: Passing GTK2_RC_FILES to the service's Environment didn't work. And
-    #   setting GTK2_RC_FILES globally in
-    #   services.xserver.displayManager.sessionCommands is too nuclear an
-    #   option. This is a clumsy workaround, but is the most targeted.
-    home.configFile."gnupg/gpg-agent.conf" = {
-      text = ''
-        default-cache-ttl ${toString cfg.cacheTTL}
-        pinentry-program ${pkgs.writeShellScriptBin "pinentry" ''
-          GTK2_RC_FILES=/etc/xdg/gtk-2.0/gtkrc exec ${pkgs.pinentry.gtk2}/bin/pinentry
-        ''}/bin/pinentry
-      '';
-    };
+    home.configFile."gnupg/gpg-agent.conf".text = ''
+      default-cache-ttl ${toString cfg.cacheTTL}
+    '';
   };
 }
