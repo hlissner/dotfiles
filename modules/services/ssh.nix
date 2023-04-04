59d8c829a1c00a4d8394deab4ccd0ad688fbe39d
Henrik Lissner <git@henrik.io>
2023-04-04 15:10:40 -0400

    fix(ssh): add host key as default IdentityFile
    Useful for server systems which each have their own key.

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -8,26 +8,35 @@ in {
     enable = mkBoolOpt false;
   };
 
-  config = mkIf cfg.enable {
-    services.openssh = {
-      enable = true;
-      settings = {
-        kbdInteractiveAuthentication = false;
-        # Require keys over passwords. Ensure target machines are provisioned
-        # with authorizedKeys!
-        passwordAuthentication = false;
-      };
-      # Suppress superfluous TCP traffic on new connections. Undo if using SSSD.
-      extraConfig = ''GSSAPIAuthentication no'';
-      # Deactivate short moduli
-      moduliFile = pkgs.runCommand "filterModuliFile" {} ''
+  config = mkMerge [
+    (mkIf cfg.enable {
+      services.openssh = {
+        enable = true;
+        settings = {
+          kbdInteractiveAuthentication = false;
+          # Require keys over passwords. Ensure target machines are provisioned
+          # with authorizedKeys!
+          passwordAuthentication = false;
+        };
+        # Suppress superfluous TCP traffic on new connections. Undo if using SSSD.
+        extraConfig = ''GSSAPIAuthentication no'';
+        # Deactivate short moduli
+        moduliFile = pkgs.runCommand "filterModuliFile" {} ''
         awk '$5 >= 3071' "${config.programs.ssh.package}/etc/ssh/moduli" >"$out"
       '';
-      # Remove all auto-generated host keys, because SSH keys have been built
-      # for all systems that require them. Systems that don't require them don't
-      # need one generated for them, and those that do should loudly fail to
-      # build if the host keys haven't been provisioned.
-      hostKeys = mkDefault [];
-    };
-  };
+        # Remove all auto-generated host keys, because SSH keys have been built
+        # for all systems that require them. Systems that don't require them don't
+        # need one generated for them, and those that do should loudly fail to
+        # build if the host keys haven't been provisioned.
+        hostKeys = mkDefault [];
+      };
+    })
+
+    {
+      programs.ssh.extraConfig = ''
+        Host *
+           ${concatMapStringsSep "\n" (key: "IdentityFile ${key.path}") config.services.openssh.hostKeys}
+      '';
+    }
+  ];
 }
