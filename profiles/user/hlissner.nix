a5fe09cde54700cc2c7bf9e6d8cdb9afdad973db
Henrik Lissner <git@henrik.io>
2023-04-07 15:56:23 -0400

    tweak(hlissner): update private settings
    And make authorized_key entry a tad more restrictive for workstations.

--- profiles/user/hlissner.nix
+++ profiles/user/hlissner.nix
@@ -1,22 +1,21 @@
 { self, lib, config, ... }:
 
 with lib;
-let pr = config.profiles.active;
-in mkMerge [
+mkMerge [
   {
     user.name = "hlissner";
     i18n.defaultLocale = mkDefault "en_US.UTF-8";
-    modules.shell.vaultwarden.config.server = "vault.home.lissner.net";
+    modules.shell.vaultwarden.config.server = "vault.lissner.net";
 
-    # Use more restrictive SSH access to workstations, which I only, ever, need
-    # local access to. Other systems, particularly servers, are always on
-    # controlled networks. There, I leave access control to the upstream router
-    # or local firewall.
+    # Be slightly more restrictive about SSH access to workstations, which I
+    # only ever need LAN access to. Other systems, particularly servers, are
+    # remoted into, and I can leave access control to the upstream router or
+    # local firewall.
     user.openssh.authorizedKeys.keys = [
-      (let key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB71rSnjuC06Qq3NLXQJwSz7jazoB+umydddrxL6vg1a";
-       in if (elem "role/workstation" pr)
-          then ''from="10.0.0.0/12" ${key}''
-          else key)
+      (let key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB71rSnjuC06Qq3NLXQJwSz7jazoB+umydddrxL6vg1a hlissner"; in
+       if (elem "role/workstation" config.profiles.active)
+       then ''from="10.0.0.0/8" ${key} hlissner''
+       else key)
     ];
   }
 
