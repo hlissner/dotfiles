6137035be6cfbf9a25d25f74609a60de8dc685c0
Henrik Lissner <git@henrik.io>
2023-04-12 19:44:08 +0200

    feat: add service module for printer sharing

---
+++ modules/services/printing.nix
@@ -0,0 +1,36 @@
+# modules/services/printing.nix
+#
+# Share the suffering.
+
+{ self, lib, config, options, ... }:
+
+with lib;
+with self.lib;
+let cfg = config.modules.services.printing;
+in {
+  options.modules.services.printing = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    services.avahi = {
+      enable = true;
+      publish = {
+        enable = true;
+        userServices = true;
+      };
+    };
+
+    services.printing = {
+      browsing = true;
+      listenAddresses = [ "*:631" ];
+      allowFrom = [ "all" ];
+      defaultShared = true;
+    };
+
+    networking.firewall = {
+      allowedUDPPorts = [ 631 ];
+      allowedTCPPorts = [ 631 ];
+    };
+  };
+}
