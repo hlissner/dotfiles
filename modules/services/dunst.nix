6f8a5a443d1b8ba7088e24452bff72afe93648ad
Henrik Lissner <git@henrik.io>
2023-04-17 12:22:12 +0200

    refactor(dunst): remove unneeded mkMerge

--- modules/services/dunst.nix
+++ modules/services/dunst.nix
@@ -22,57 +22,53 @@ in {
     settings = mkOpt (attrsOf (attrsOf eitherStrBoolIntList)) {};
   };
 
-  config = mkIf cfg.enable (mkMerge [
-    {
-      environment.systemPackages = [ pkgs.dunst ];
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.dunst ];
 
-      systemd.user.services.dunst = {
-        enable = true;
-        unitConfig = {
-          Description = "Dunst notification daemon";
-          After  = [ "graphical-session-pre.target" ];
-          PartOf = [ "graphical-session.target" ];
-        };
-        serviceConfig = {
-          Type = "dbus";
-          BusName = "org.freedesktop.Notifications";
-          ExecStart = "${pkgs.dunst}/bin/dunst";
-        };
+    systemd.user.services.dunst = {
+      enable = true;
+      unitConfig = {
+        Description = "Dunst notification daemon";
+        After  = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
       };
-
-      home.dataFile."dbus-1/services/org.knopwob.dunst.service".source =
-        "${pkgs.dunst}/share/dbus-1/services/org.knopwob.dunst.service";
-    }
-
-    (mkIf (cfg.settings != {}) {
-      home.configFile."dunst/dunstrc" = {
-        text = toDunstIni (mergeAttrs' [
-          {
-            global = {
-              title = "Dunst";
-              class = "Dunst";
-              browser = "firefox -new-tab";
-              dmenu = "rofi -dmenu -p dunst";
-              history_length = 20;
-              idle_threshold = 120;
-              markup = "full";
-              sort = true;
-              sticky_history = true;
-              progress_bar = true;
-              indicate_hidden = true;
-              notification_limit = 0;
-              monitor = 0;
-            };
-            experimental = {
-              enable_recursive_icon_lookup = true;
-            };
-          }
-          cfg.settings
-        ]);
-        onChange = ''
-          ${pkgs.procps}/bin/pkill -u "$USER" ''${VERBOSE+-e} dunst || true
-        '';
+      serviceConfig = {
+        Type = "dbus";
+        BusName = "org.freedesktop.Notifications";
+        ExecStart = "${pkgs.dunst}/bin/dunst";
       };
-    })
-  ]);
+    };
+
+    home.dataFile."dbus-1/services/org.knopwob.dunst.service".source =
+      "${pkgs.dunst}/share/dbus-1/services/org.knopwob.dunst.service";
+
+    home.configFile."dunst/dunstrc" = {
+      text = toDunstIni (mergeAttrs' [
+        {
+          global = {
+            title = "Dunst";
+            class = "Dunst";
+            browser = "firefox -new-tab";
+            dmenu = "rofi -dmenu -p dunst";
+            history_length = 20;
+            idle_threshold = 120;
+            markup = "full";
+            sort = true;
+            sticky_history = true;
+            progress_bar = true;
+            indicate_hidden = true;
+            notification_limit = 0;
+            monitor = 0;
+          };
+          experimental = {
+            enable_recursive_icon_lookup = true;
+          };
+        }
+        cfg.settings
+      ]);
+      onChange = ''
+        ${pkgs.procps}/bin/pkill -u "$USER" ''${VERBOSE+-e} dunst || true
+      '';
+    };
+  };
 }
