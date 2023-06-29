ed53b3906fbaf2ff2a676029d454b0e02d8d1c53
Henrik Lissner <git@henrik.io>
2023-06-30 01:07:46 +0200

    fix: move boot.{tmpOnTmpfs,cleanTmpDir} -> boot.tmp.*
    This was changed upstream, at some point, in 23.05.

--- modules/security.nix
+++ modules/security.nix
@@ -8,10 +8,10 @@
 
   # tmpfs = /tmp is mounted in ram. Doing so makes temp file management speedy
   # on ssd systems and more secure (and volatile)! Because it's wiped on reboot.
-  boot.tmpOnTmpfs = lib.mkDefault true;
+  boot.tmp.useTmpfs = lib.mkDefault true;
   # If not using tmpfs, which is naturally purged on reboot, we must clean it
   # /tmp ourselves. /tmp should be volatile storage!
-  boot.cleanTmpDir = lib.mkDefault (!config.boot.tmpOnTmpfs);
+  boot.tmp.cleanOnBoot = lib.mkDefault (!config.boot.tmp.useTmpfs);
 
   # Fix a security hole in place for backwards compatibility. See desc in
   # nixpkgs/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix
