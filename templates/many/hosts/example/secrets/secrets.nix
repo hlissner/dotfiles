68b25bbb2168d1495ca2c0591df90725f6c44d78
Henrik Lissner <git@henrik.io>
2024-06-22 02:15:22 -0400

    refactor: remove templates
    I'll redo these some other time.

--- templates/many/hosts/example/secrets/secrets.nix
+++
@@ -1,6 +0,0 @@
-let key = "ssh-ed25519 ...";
-in {
-  # "fileA.age".publicKeys = [key];
-  # "fileB.age".publicKeys = [key];
-  # "fileC.age".publicKeys = [key];
-}
--- templates/one/secrets/secrets.nix
+++
@@ -1,6 +0,0 @@
-let key = "ssh-ed25519 ...";
-in {
-  # "file1.age".publicKeys = [key];
-  # "file2.age".publicKeys = [key];
-  # "file3.age".publicKeys = [key];
-}
