2e7486e6d3c2ba5fce86375a3fd88ffb840f554a
Henrik Lissner <git@henrik.io>
2024-06-22 02:13:13 -0400

    feat: add modules.services.ydotool

---
+++ modules/services/ydotool.nix
@@ -0,0 +1,73 @@
+# modules/services/ydotool.nix
+#
+# Used as a more portable alternative to xdotool. Not a big fan of anything that
+# needs a daemon though; more so since ydotool will soon be rewritten in
+# javascript (for its history of supply chain vulnerabiltiies), so I'll likely
+# drop it once wtype adds mouse support (see atx/wtype#24).
+
+{ self, lib, config, options, pkgs, ... }:
+
+with lib;
+with self.lib;
+let cfg = config.modules.services.ydotool;
+    ydotool = pkgs.ydotool;
+    socket = "/run/ydotoold/socket";
+in {
+  options.modules.services.ydotool = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    environment.variables.YDOTOOL_SOCKET = socket;
+
+    users.groups.ydotool = {};
+    user.extraGroups = [ "ydotool" ];
+
+    environment.systemPackages = [ ydotool ];
+    systemd.services.ydotoold = {
+      wantedBy = [ "multi-user.target" ];
+      partOf = [ "multi-user.target" ];
+      serviceConfig = {
+        Group = "ydotool";
+        RuntimeDirectory = "ydotoold";
+        RuntimeDirectoryMode = "0750";
+        ExecStart = "${getExe' ydotool "ydotoold"} --socket-path=${socket} --socket-perm=0660";
+        # hardening
+        ## allow access to uinput
+        DeviceAllow = [ "/dev/uinput" ];
+        DevicePolicy = "closed";
+        ## allow creation of unix sockets
+        RestrictAddressFamilies = [ "AF_UNIX" ];
+        CapabilityBoundingSet = "";
+        IPAddressDeny = "any";
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        PrivateNetwork = true;
+        PrivateTmp = true;
+        PrivateUsers = true;
+        ProcSubset = "pid";
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectProc = "invisible";
+        ProtectSystem = "strict";
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallArchitectures = "native";
+        SystemCallFilter = [
+          "@system-service"
+          "~@privileged"
+          "~@resources"
+        ];
+        UMask = "0077";
+        # -> systemd-analyze security score 0.7 SAFE
+      };
+    };
+  };
+}
