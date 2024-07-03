8339d668e6cddc0f54729a1005dda9712c10008e
Henrik Lissner <git@henrik.io>
2024-07-03 00:15:07 -0400

    fix(fail2ban): update for 24.05

--- modules/services/fail2ban.nix
+++ modules/services/fail2ban.nix
@@ -11,28 +11,27 @@ in {
   config = mkIf cfg.enable {
     services.fail2ban = {
       enable = true;
-      ignoreIP = [ "127.0.0.1/16" "192.168.1.0/24" ];
-      banaction-allports = "iptables-allports";
+      ignoreIP = [ "127.0.0.1/16" "192.168.0.0/16" ];
+      bantime = "1h";
       bantime-increment = {
         enable = true;
         maxtime = "168h";
         factor = "4";
       };
-      jails.DEFAULT = ''
-        blocktype = DROP
-        bantime = 1h
-        findtime = 1h
-      '';
+      jails.DEFAULT.settings = {
+        blocktype = "DROP";
+        findtime = "1h";
+      };
     };
 
     # Extra filters
-    environment.etc = {
-      "fail2ban/filter.d/gitea.conf".text = ''
-        [Definition]
-        failregex =  .*(Failed authentication attempt|invalid credentials|Attempted access of unknown user).* from <HOST>
-        ignoreregex =
-        journalmatch = _SYSTEMD_UNIT=gitea.service
-      '';
-    };
+    # environment.etc = {
+    #   "fail2ban/filter.d/gitea.conf".text = ''
+    #     [Definition]
+    #     failregex =  .*(Failed authentication attempt|invalid credentials|Attempted access of unknown user).* from <HOST>
+    #     ignoreregex =
+    #     journalmatch = _SYSTEMD_UNIT=gitea.service
+    #   '';
+    # };
   };
 }
