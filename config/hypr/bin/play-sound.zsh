be5fb3681a478db3d0339b099abd8dd97157edd7
Henrik Lissner <git@henrik.io>
2024-07-23 23:02:51 -0400

    refactor(hypr): make bin/play-sound more robust
    And add a ls subcommand.

--- config/hypr/bin/play-sound.zsh
+++ config/hypr/bin/play-sound.zsh
@@ -7,15 +7,22 @@
 # DESCRIPTION:
 #   TODO
 
-[[ $(makoctl mode) == dnd ]] && return
-
-hey.requires play
-
-# TODO: Abstract sound library
-zparseopts -E -D -F -- v:=volume || exit 1
-local file=$(echo $(hey path theme sounds)/$1.{ogg,wav,mp3}(-.N[1]))
-if [[ -z $file ]]; then
-  hey.error "Unrecognized sound: $1"
-  exit 1
+if (( $# == 0 )); then
+  hey.error "Name of sound required"
+  exit 2
 fi
-hey.do play -q $volume $file &
+
+local dir=$(hey path theme sounds)
+if [[ "$1" == "ls" ]]; then
+  ls -l "$dir"
+else
+  [[ $(makoctl mode) == dnd ]] && return
+  hey.requires play
+  zparseopts -E -D -F -- v:=volume || exit 1
+  local file=$(echo "$dir"/$1.{ogg,wav,mp3}(-.N[1]))
+  if [[ -z $file ]]; then
+    hey.error "Unrecognized sound: $1"
+    exit 1
+  fi
+  hey.do play -q $volume $file &
+fi
