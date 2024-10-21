0dc6f397892e2a6d2d513358c1cdc9ad52c8c21b
Henrik Lissner <git@henrik.io>
2024-10-20 21:27:04 -0400

    fix(bluetooth): restart BT after wake from suspend
    Ref: https://gist.github.com/samueldr/356e65374d452e4fd45314f818ae3545

--- modules/profiles/hardware/bluetooth.nix
+++ modules/profiles/hardware/bluetooth.nix
@@ -8,4 +8,11 @@ with lib;
 with hey.lib;
 mkIf (elem "bluetooth" config.modules.profiles.hardware) {
   hardware.bluetooth.enable = true;
+
+  # Brute force a reset after waking up from sleep, as some bluetooth devices
+  # will fail to connect to a system that's been suspended at some point.
+  powerManagement.resumeCommands = ''
+    ${pkgs.util-linux}/bin/rfkill block bluetooth
+    ${pkgs.util-linux}/bin/rfkill unblock bluetooth
+  '';
 }
