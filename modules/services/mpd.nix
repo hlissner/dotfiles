254aea2230e1350409a7ae7b6566bcd98f5b5360
Henrik Lissner <git@henrik.io>
2025-05-14 19:05:01 +0200

    feat(services): add basic mpd module
    More to come later.

---
+++ modules/services/mpd.nix
@@ -0,0 +1,18 @@
+# TODO: Incomple
+
+{ hey, lib, config, options, pkgs, ... }:
+
+with lib;
+with hey.lib;
+let cfg = config.modules.services.dunst;
+in {
+  options.modules.services.mpd = with types; {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    services.mpd = {
+      enable = true;
+    };
+  };
+}
