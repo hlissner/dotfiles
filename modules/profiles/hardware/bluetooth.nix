53c58ac14a40e10afe66e1af192b28c706c82906
Henrik Lissner <git@henrik.io>
2025-10-07 15:05:20 -0400

    tweak(profiles/bluetooth): ControllerMode = bredr
    Resolves some connectivity issues with my BT earphones.

--- modules/profiles/hardware/bluetooth.nix
+++ modules/profiles/hardware/bluetooth.nix
@@ -9,6 +9,11 @@ with hey.lib;
 mkIf (elem "bluetooth" config.modules.profiles.hardware) {
   hardware.bluetooth.enable = true;
 
+  hardware.bluetooth.settings = {
+    General.ControllerMode = "bredr";
+    Policy.ReconnectAttempts = 0;
+  };
+
   # Brute force a reset after waking up from sleep, as some bluetooth devices
   # will fail to connect to a system that's been suspended at some point.
   powerManagement.resumeCommands = ''
