9dbe3a62865cf51b9982236d52df271c93e4f013
Henrik Lissner <git@henrik.io>
2025-10-23 02:42:11 -0400

    fix(ssh): conditionally create $XDG_CONFIG_HOME/ssh

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -8,36 +8,40 @@ in {
     enable = mkBoolOpt false;
   };
 
-  config = mkIf cfg.enable {
-    # Ensure this directory exists and has correct permissions.
-    systemd.user.tmpfiles.rules = [ "d %h/.config/ssh 700 - - - -" ];
-
-    services.openssh = {
-      enable = true;
-      settings = {
-        KbdInteractiveAuthentication = false;
-        # Require keys over passwords. Ensure target machines are provisioned
-        # with authorizedKeys!
-        PasswordAuthentication = false;
+  config = mkIf cfg.enable (mkMerge [
+    {
+      services.openssh = {
+        enable = true;
+        settings = {
+          KbdInteractiveAuthentication = false;
+          # Require keys over passwords. Ensure target machines are provisioned
+          # with authorizedKeys!
+          PasswordAuthentication = false;
+        };
+        # Suppress superfluous TCP traffic on new connections. Undo if using SSSD.
+        extraConfig = ''GSSAPIAuthentication no'';
+        # Deactivate short moduli
+        moduliFile = pkgs.runCommand "filterModuliFile" {} ''
+          awk '$5 >= 3071' "${config.programs.ssh.package}/etc/ssh/moduli" >"$out"
+        '';
+        # Removes the default RSA key (not that it represents a vulnerability, per
+        # se, but is one less key (that I don't plan to use) to the castle laying
+        # around) and improves the ed25519 key's entropy by generating it with 100
+        # rounds (default is 16).
+        hostKeys = [
+          {
+            comment = "${config.networking.hostName}.local";
+            path = "/etc/ssh/ssh_host_ed25519_key";
+            rounds = 100;
+            type = "ed25519";
+          }
+        ];
       };
-      # Suppress superfluous TCP traffic on new connections. Undo if using SSSD.
-      extraConfig = ''GSSAPIAuthentication no'';
-      # Deactivate short moduli
-      moduliFile = pkgs.runCommand "filterModuliFile" {} ''
-        awk '$5 >= 3071' "${config.programs.ssh.package}/etc/ssh/moduli" >"$out"
-      '';
-      # Removes the default RSA key (not that it represents a vulnerability, per
-      # se, but is one less key (that I don't plan to use) to the castle laying
-      # around) and improves the ed25519 key's entropy by generating it with 100
-      # rounds (default is 16).
-      hostKeys = [
-        {
-          comment = "${config.networking.hostName}.local";
-          path = "/etc/ssh/ssh_host_ed25519_key";
-          rounds = 100;
-          type = "ed25519";
-        }
-      ];
-    };
-  };
+    }
+
+    (mkIf config.modules.xdg.ssh.enable {
+      # Ensure this directory exists and has correct permissions.
+      systemd.user.tmpfiles.rules = [ "d %h/.config/ssh 700 - - - -" ];
+    })
+  ]);
 }
