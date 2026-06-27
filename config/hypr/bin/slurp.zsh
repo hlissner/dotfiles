9207514e7a22850697e4e5acc4ad2f7c3e025e30
Henrik Lissner <git@henrik.io>
2026-06-27 18:11:36 -0400

    feat(hypr): slurp: add last subcommand

--- config/hypr/bin/slurp.zsh
+++ config/hypr/bin/slurp.zsh
@@ -8,23 +8,36 @@
 #   Extends slurp to more closely mirror slop's interface. Passes all arguments
 #   to slurp.
 
-case $1 in
-  '') slurp $@ ;;
+main() {
+  hey.requires slurp
+  case $1 in
+    '') slurp $@ ;;
+    output)
+      hey set $cache_key "-o ${@:2}"
+      slurp -o ${@:2}
+      ;;
+    region)
+      slurp ${@:2}
+      ;;
+    window)
+      local cond_str
+      for id in $(hyprctl -i 0 monitors -j | jq '.[] | .activeWorkspace.id'); do
+        cond_str="${cond_str}${cond_str:+ or }.workspace.id == $id"
+      done
+      hyprctl clients -j | jq -r ".[] | select($cond_str) | .at,.size" \
+        | jq -s 'add | range(0; length; 4) as $i | .[$i:$i+4]' \
+        | jq -r '"\(.[0]),\(.[1]) \(.[2])x\(.[3])"' \
+        | slurp -r ${@:2}
+      ;;
+  esac
+}
 
-  output)
-    slurp -o ${@:2}
-    ;;
-  region)
-    slurp ${@:2}
-    ;;
-  window)
-    local cond_str
-    for id in $(hyprctl -i 0 monitors -j | jq '.[] | .activeWorkspace.id'); do
-      cond_str="${cond_str}${cond_str:+ or }.workspace.id == $id"
-    done
-    hyprctl clients -j | jq -r ".[] | select($cond_str) | .at,.size" \
-                       | jq -s 'add | range(0; length; 4) as $i | .[$i:$i+4]' \
-                       | jq -r '"\(.[0]),\(.[1]) \(.[2])x\(.[3])"' \
-                       | slurp -r ${@:2}
-    ;;
-esac
+local cache_key=hypr.slurp.last
+if [[ "$1" == last ]]; then
+  if ! hey get $cache_key; then
+    notify-send -u critical "No previous screenshot selection to retake"
+    exit 1
+  fi
+else
+  hey set $cache_key "$(main "$@")"
+fi
