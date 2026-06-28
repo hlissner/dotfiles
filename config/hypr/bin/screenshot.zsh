f6bd80beaf7ffd482ae7a45ff04e06afdbbd85cb
Henrik Lissner <git@henrik.io>
2026-06-28 01:00:53 -0400

    feat(hypr): screenshot.zsh: optimize png result
    What I want is to optimize the png coming out of swappy and going into
    my clipboard, but *only* if I intentionally ctrl-c from swappy. If I ESC
    or ctrl-s, I want to abort. This change facilitates that workflow, and
    as fast as I can make it happen.

--- config/hypr/bin/screenshot.zsh
+++ config/hypr/bin/screenshot.zsh
@@ -11,8 +11,21 @@
 #   slurp+grim+swappy stack.
 
 main() {
-  hey.requires slurp grim swappy
-  hey.do grim -t ppm -g "$(hey .slurp ${1:-region})" - | swappy -f -
+  set -eo pipefail
+  hey.requires slurp grim swappy pngquant
+  wl-copy --clear  # don't process PNGs already in clipboard
+  # ppm is fastest. I leave it to swappy to generate an optimized png
+  if grim -t ppm -g "$(hey .slurp ${1:-region})" - | swappy -f -; then
+    if [[ "$(wl-paste --list-types | grep -Fx 'image/png')" ]]; then
+      if wl-paste --type image/png \
+        | pngquant -Q 90-95 -s 1 -f - \
+        | wl-copy --type image/png; then
+        dms ipc toast info "Copied screenshot to clipboard"
+      fi
+    fi
+  else
+    dms ipc toast warn "Aborted"
+  fi
 }
 
 main $@
