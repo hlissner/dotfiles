79c6ae26a9cdf1f47d5e57e494b30fe7f91c5ee4
Henrik Lissner <git@henrik.io>
2026-09-20 06:30:34 -0400

    refactor(zsh): onReload: show what area is running
    And be less verbose about every single file that's deleted (too spammy).

--- config/zsh/hooks/onReload.zsh
+++ config/zsh/hooks/onReload.zsh
@@ -4,17 +4,18 @@
 # SYNOPSIS:
 #   hey reload
 #
-# SYNOPSIS:
-#   Triggered by 'hey reload'
+# DESCRIPTION:
+#   Deletes zpm's init file, ZSH's cache, and all compiled zsh scripts. Next
+#   time you start up a zsh shell it'll rebuild all it needs.
 
-echo "Deleting zsh cache files..."
-rm -frv "$XDG_CACHE_HOME"/zsh/*(DN)
+echo "zsh: deleting zsh cache files..."
+rm -fr "$XDG_CACHE_HOME"/zsh/*(DN)
 
-echo "Deleting compiled zsh files..."
-rm -fv "$ZDOTDIR"/**/*.zwc(D.N)
-rm -fv "${DOTFILES_HOME:-$HOME/.config/dotfiles}"/config/zsh/**/*.zwc(D.N)
+echo "zsh: deleting compiled zsh files..."
+rm -f "$ZDOTDIR"/**/*.zwc(D.N)
+rm -f "${DOTFILES_HOME:-$HOME/.config/dotfiles}"/config/zsh/**/*.zwc(D.N)
 
 # Can't use `zpm clean` because these hooks can run outside an interactive shell
 # where zpm's functions aren't loaded.
-echo "Resetting zpm..."
-rm -frv "${ZSH_TMP_DIR:-${TMPDIR:-/tmp}/zsh-${UID:-user}}"
+echo "zsh: resetting zpm..."
+rm -fr "${ZSH_TMP_DIR:-${TMPDIR:-/tmp}/zsh-${UID:-user}}"
